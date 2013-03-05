33fb2279203b4a8706aa8f68ad9967ec97855f73
Keith Smiley <keithbsmiley@gmail.com>
2013-03-04 23:07:00 -0500

    Added reload to manage commands

--- manage.sh
+++ manage.sh
@@ -128,8 +128,10 @@ fi
 # Check whether the user is installing or removing
 if [[ $1 == "install" ]]; then
 	install_links
+  vim +BundleInstall +qall
 elif [[ $1 == "remove" ]]; then
 	remove_links
 else
 	die
 fi
+
