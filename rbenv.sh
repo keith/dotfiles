1b3bfc17bcee6443a2f5fd6670ed6ddc10887f75
Keith Smiley <keithbsmiley@gmail.com>
2013-07-04 17:06:03 -0400

    rbenv install/remove script

---
+++ rbenv.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+function install () {
+    git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+    git clone https://github.com/sstephenson/rbenv-default-gems.git $HOME/.rbenv/plugins/rbenv-default-gems
+    git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+    git clone https://github.com/sstephenson/rbenv-gem-rehash.git $HOME/.rbenv/plugins/rbenv-gem-rehash
+    git clone https://github.com/tpope/rbenv-readline.git $HOME/.rbenv/plugins/rbenv-readline
+
+    ln -s $(pwd)/default-gems $HOME/.rbenv
+}
+
+function die () {
+    echo "Usage ./rbenv.sh {install|remove}"
+    exit
+}
+
+if [[ $# != 1 ]]; then
+    die
+fi
+
+if [[ $1 == "install" ]]; then
+    install
+elif [[ $1 == "remove" ]]; then
+    rm -rf $HOME/.rbenv
+else
+    die
+fi
+
