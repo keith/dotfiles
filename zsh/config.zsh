42dd0ae4fc5583fb06bb32cd5ae484b487e87364
Keith Smiley <keithbsmiley@gmail.com>
2013-08-06 15:45:22 -0400

    Setup zsh config seperately

---
+++ zsh/config.zsh
@@ -0,0 +1,44 @@
+# Set prompt to % for users and # for root
+export PS1='%# '
+
+fpath=($DOTFILES/functions $fpath)
+autoload -Uz $DOTFILES/functions/*(:t)
+
+# Attempt to correct spelling
+setopt CORRECT
+
+# Do completions from anywhere in the word
+setopt COMPLETE_IN_WORD
+
+
+# History settings
+# Save x items to the given history file
+HISTSIZE=1000
+SAVEHIST=1000
+HISTFILE=$HOME/.zsh_history
+
+# Append history to the zsh_history file
+setopt APPEND_HISTORY
+
+# Adds history incrementally and share it across sessions
+setopt INC_APPEND_HISTORY SHARE_HISTORY 
+
+# Ignore duplicates in zsh history
+setopt HIST_IGNORE_ALL_DUPS
+
+# Ignore commands for history that start with a space
+setopt HIST_IGNORE_SPACE
+
+# Remove superfluous blanks from each command line being added to the history list
+setopt HIST_REDUCE_BLANKS
+
+
+# Use vim shortcuts within the terminal (defaults to insert mode)
+bindkey -v
+
+# Restore 'normal' search in VI mode
+bindkey '^R' history-incremental-search-backward
+
+# Allow alt/option . to insert the argument from the previous command
+bindkey '\e.' insert-last-word
+
