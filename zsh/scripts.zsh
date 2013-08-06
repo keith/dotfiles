cdad67e3d254a5b23b7ec422f32a48181f1c839a
Keith Smiley <keithbsmiley@gmail.com>
2013-08-06 15:55:12 -0400

    Separate scripts file

---
+++ zsh/scripts.zsh
@@ -0,0 +1,13 @@
+# Path to script files in dotfiles directory
+export SCRIPT_PATH="$DOTFILES/scripts"
+
+# Scripts
+alias append="$SCRIPT_PATH/fileAppend.rb"
+alias cloudfront="$SCRIPT_PATH/cloudFront.rb"
+alias lc="$SCRIPT_PATH/license.rb"
+alias methods="$SCRIPT_PATH/methods.rb"
+alias mksite="$SCRIPT_PATH/newSite.rb"
+alias pf="$SCRIPT_PATH/processfile.rb"
+alias rename="$SCRIPT_PATH/imageRenamer.rb"
+alias xc="$SCRIPT_PATH/xcworkspace.rb"
+
