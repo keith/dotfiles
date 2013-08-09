544dd094e310550563fcb50e8a2189de8d109495
Keith Smiley <keithbsmiley@gmail.com>
2013-08-08 23:27:13 -0400

    Fix ubuntu install commands

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -4,9 +4,12 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install xclip vim zsh tmux
+sudo -y apt-get upgrade
+sudo -y apt-get install tmux vim zsh 
 
 chsh -s $(which zsh) $USER
-chsh -s $(which zsh) $SUDO_USER
+if [[ ! -z "$SUDO_USER" ]];then
+    chsh -s $(which zsh) $SUDO_USER
+fi
+
 
