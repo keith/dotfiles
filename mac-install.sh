7f6222a18c11ac1009bb7e4a8afa59f083d30025
Keith Smiley <keithbsmiley@gmail.com>
2013-08-08 22:58:53 -0400

    Install mac git credential helper

--- mac-install.sh
+++ mac-install.sh
@@ -43,5 +43,9 @@ done
 
 brew install --HEAD brew-cask
 
+curl -s -O http://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
+chmod u+x git-credential-osxkeychain
+sudo mv git-credential-osxkeychain "$(dirname $(which git))/git-credential-osxkeychain"
+
 ./rbenv.sh install
 
