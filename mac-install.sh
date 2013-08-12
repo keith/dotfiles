56cd8209e502a41162242243a31a9f1e453c4afd
Keith Smiley <keithbsmiley@gmail.com>
2013-08-12 15:17:06 -0400

    Install nodejs globals

--- mac-install.sh
+++ mac-install.sh
@@ -6,16 +6,14 @@ if ! which xcodebuild &> /dev/null; then
     return
 fi
 
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" &
-wait
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
 if [[ ! -e "manage.sh" ]]; then
     echo "Make sure you have the manage script in the same directory!"
     return
 fi
 
-./manage.sh install &
-wait
+./manage.sh install
 
 if [[ ! -e "$HOME/.bashrc" ]]; then
     echo "Looks like the manage script failed, try and run it manually"
@@ -31,8 +29,7 @@ fi
 
 brew tap homebrew/dupes
 brew tap Keithbsmiley/homebrew-cask
-brew update &
-wait
+brew update
 
 formulas=(bash cloc git grc grep heroku-toolbelt hub imagemagick less llvm macvim mercurial mogenerator node openssh openssl python python3 readline reattach-to-user-namespace sqlite the_silver_searcher tmux tree valgrind vim wget zsh zsh-completions)
 
@@ -46,6 +43,13 @@ brew install --HEAD brew-cask
 curl -s -O http://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
 chmod u+x git-credential-osxkeychain
 sudo mv git-credential-osxkeychain "$(dirname $(which git))/git-credential-osxkeychain"
+
+nodes=(bower coffee-script)
+
+for n in ${nodes[@]}
+do
+    npm install -g $n
+done
 
 ./rbenv.sh install
 
