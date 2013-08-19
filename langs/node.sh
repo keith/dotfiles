2321e6dce55e2e11ed50eba42b7842a4cfffe820
Keith Smiley <keithbsmiley@gmail.com>
2013-08-19 13:47:16 -0400

    Add node install functions like rbenv.sh

--- langs/node.sh
+++ langs/node.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 
+function install () {
+    nodes=(bower coffee-script grunt-cli)
+
+    for n in ${nodes[@]}
+    do
+        npm install -g $n
+    done
+}
+
+function die () {
+    echo "Usage ./$(basename $0) install"
+    exit
+}
+
 if ! which npm &> /dev/null;then
     echo "You must install node before installing its packages"
     exit
 fi
 
-nodes=(bower coffee-script grunt-cli)
+if [[ $# != 1 ]]; then
+    die
+fi
 
-for n in ${nodes[@]}
-do
-    npm install -g $n
-done
+if [[ $1 == "install" ]]; then
+    install
+else
+    die
+fi
 
