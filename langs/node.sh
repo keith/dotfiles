07d92e6134a554274af1e3ffc6f1ed8ee61cba3f
Keith Smiley <keithbsmiley@gmail.com>
2013-08-19 13:36:44 -0400

    Move npm installs to seperate script

---
+++ langs/node.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if which npm &> /dev/null;then
+    echo "You must install node before installing its packages"
+    exit
+fi
+
+nodes=(bower coffee-script grunt-cli)
+
+for n in ${nodes[@]}
+do
+    npm install -g $n
+done
+
