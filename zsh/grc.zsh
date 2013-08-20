9eb727b7b791b81b56a42589d1d95d39e7104dde
Keith Smiley <keithbsmiley@gmail.com>
2013-08-20 09:30:29 -0400

    Reorganize grc config command

--- zsh/grc.zsh
+++ zsh/grc.zsh
@@ -1,6 +1,5 @@
 # GRC colorizes nifty unix tools all over the place
-if $(grc &>/dev/null) && ! $(brew &>/dev/null)
-then
+if which grc &> /dev/null && which brew &> /dev/null;then
   source $(brew --prefix)/etc/grc.bashrc
 fi
 
