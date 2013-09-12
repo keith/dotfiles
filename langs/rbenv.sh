1200b8f2262d11ee310fef4c545f9e9aa4746313
John Hiott <jhiott@oolcay.com>
2013-09-12 11:04:24 -0400

    Added paths for rbenv and rbenv init

--- langs/rbenv.sh
+++ langs/rbenv.sh
@@ -15,6 +15,10 @@ function install () {
     git clone https://github.com/tpope/rbenv-readline.git $rbenvpath/plugins/rbenv-readline
 
     ln -s $DOTFILES/default-gems $rbenvpath
+    
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+	echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+	exec $SHELL -l
 }
 
 function die () {
