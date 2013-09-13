0a248d3e9db8dcba9cdffb9d7e2bbcf0d1df061d
Keith Smiley <keithbsmiley@gmail.com>
2013-09-13 14:23:58 -0400

    Add silversearcher install

--- linux/ubuntu-install.sh
+++ linux/ubuntu-install.sh
@@ -4,10 +4,11 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 sudo add-apt-repository ppa:gnome-terminator
+sudo apt-add-repository ppa:mizuno-as/silversearcher-ag
 
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install curl subversion libcurl3 libcurl3-dev php5-curl gconf-editor zlib1g-dev openssl libopenssl-ruby1.9.1 libssl-dev build-essential libruby1.9.1 libreadline-dev git-core git clang gnustep gnustep-devel terminator tmux vim vim-gnome zsh
+sudo apt-get -y install curl subversion libcurl3 libcurl3-dev php5-curl gconf-editor zlib1g-dev openssl libopenssl-ruby1.9.1 libssl-dev build-essential libruby1.9.1 libreadline-dev git-core git clang silversearcher-ag terminator tmux vim vim-gnome zsh
 
 chsh -s $(which zsh) $USER
 if [[ ! -z "$SUDO_USER" ]];then
