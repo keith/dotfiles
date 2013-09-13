904a3276a6fb0ee0025eba7d965f418b4811a14d
Keith Smiley <keithbsmiley@gmail.com>
2013-09-13 15:44:29 -0400

    Add script for linking linux configs

---
+++ linux/configs.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+FILES=(terminator)
+p=$HOME/.config
+dir=$DOTFILES/linux/configs
+
+function link() {
+    local folder=$1
+
+    if [[ ! -d $dir/$folder ]];then
+        echo "$folder doesn't exist"
+        return
+    fi
+
+    local path=$p
+    if [[ ! -d $path/$folder ]];then
+        echo "Linking $dir/$folder to $path"
+        ln -s $dir/$folder $path
+    fi
+}
+
+function new_path() {
+    local folder=$1
+    echo "$p/$folder"
+}
+
+function install_links() {
+    for FILE in ${FILES[@]}
+    do
+        link $FILE
+    done
+}
+
+if [[ ! -d "$p" ]];then
+    mkdir -p $p
+fi
+
+install_links
+echo "Done linking configs"
+
