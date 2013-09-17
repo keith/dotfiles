a95773c8eda4e7e0ad99a309cf976131f47cd616
Keith Smiley <keithbsmiley@gmail.com>
2013-09-17 16:16:47 -0400

    Add nodejs install for ubuntu

--- linux/ubuntu-install.sh
+++ linux/ubuntu-install.sh
@@ -5,14 +5,14 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 sudo add-apt-repository ppa:gnome-terminator
 sudo apt-add-repository ppa:mizuno-as/silversearcher-ag
+sudo add-apt-repository ppa:chris-lea/node.js
 
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install curl subversion libcurl3 libcurl3-dev php5-curl gconf-editor zlib1g-dev openssl libopenssl-ruby1.9.1 libssl-dev build-essential libruby1.9.1 libreadline-dev git-core git clang silversearcher-ag terminator tmux vim vim-gnome zsh xclip exuberant-ctags mutt-patched
+sudo apt-get -y install curl subversion libcurl3 libcurl3-dev php5-curl gconf-editor zlib1g-dev openssl libopenssl-ruby1.9.1 libssl-dev build-essential libruby1.9.1 libreadline-dev git-core git clang silversearcher-ag terminator tmux vim vim-gnome zsh xclip exuberant-ctags mutt-patched python-software-properties python g++ make nodejs
 
 chsh -s $(which zsh) $USER
 if [[ ! -z "$SUDO_USER" ]];then
     chsh -s $(which zsh) $SUDO_USER
 fi
-
 
