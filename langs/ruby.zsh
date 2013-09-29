d74a2049f98f839385b74a30f80162f6a699f49c
Keith Smiley <keithbsmiley@gmail.com>
2013-09-29 04:02:01 +0000

    First pass at conditional RVM setup

--- langs/ruby.zsh
+++ langs/ruby.zsh
@@ -1,24 +1,30 @@
 # The path of rbenv
 local RBENVPATH="$HOME/.rbenv"
+local HAS_RUBY=false
 
-if [[ ! -d $RBENVPATH ]];then
-  echo "rbenv is not installed. Run '$(dirname $0)/rbenv.sh install'"
-  return
+if [[ -d $RBENVPATH ]];then
+  # Add rbenv to PATH
+  PATH="$RBENVPATH/bin:$PATH"
+
+  # Load rbenv on launch
+  eval "$(rbenv init -)"
+
+  HAS_RUBY=true
 fi
 
-# Add rbenv to PATH
-PATH="$RBENVPATH/bin:$PATH"  
+local RVMPATH="$HOME/.rvm"
 
-# Load rbenv on launch
-eval "$(rbenv init -)"
+if [[ $HAS_RUBY == false && -d $RVMPATH ]];then
+  PATH="$RVMPATH/bin:$PATH"
+fi
 
-# Ruby/Rails
-alias ruby="ruby -w"
-alias bi="bundle install"
-alias bdgem="gem build *.gemspec; gem install *.gem --no-ri --no-rdoc; rbenv rehash"
-alias rr="rbenv rehash"
-alias rake="noglob rake"
-alias binstubs="gem regenerate_binstubs"
-alias coveron="export COVERALLS_RUN_LOCALLY=true"
-alias coveroff="unset COVERALLS_RUN_LOCALLY"
+if $HAS_RUBY;then
+  # Ruby/Rails
+  alias bi="bundle install"
+  alias bdgem="gem build *.gemspec; gem install *.gem --no-ri --no-rdoc; rbenv rehash"
+  alias rake="noglob rake"
+  alias binstubs="gem regenerate_binstubs"
+  alias coveron="export COVERALLS_RUN_LOCALLY=true"
+  alias coveroff="unset COVERALLS_RUN_LOCALLY"
+fi
 
