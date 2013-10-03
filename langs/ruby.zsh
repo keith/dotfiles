2c5b6aee47ef21db2a3c0eec89adf3531820a716
Keith Smiley <keithbsmiley@gmail.com>
2013-10-02 20:57:14 -0400

    Update ruby.zsh for rbenv or rvm paths

--- langs/ruby.zsh
+++ langs/ruby.zsh
@@ -1,30 +1,19 @@
-# The path of rbenv
 local RBENVPATH="$HOME/.rbenv"
-local HAS_RUBY=false
-
-if [[ -d $RBENVPATH ]];then
-  # Add rbenv to PATH
-  PATH="$RBENVPATH/bin:$PATH"
-
-  # Load rbenv on launch
-  eval "$(rbenv init -)"
-
-  HAS_RUBY=true
-fi
-
 local RVMPATH="$HOME/.rvm"
 
-if [[ $HAS_RUBY == false && -d $RVMPATH ]];then
+if [[ -d $RBENVPATH ]];then
+  PATH="$RBENVPATH/bin:$PATH"
+  eval "$(rbenv init -)"
+elif [[ -d $RVMPATH ]];then
   PATH="$RVMPATH/bin:$PATH"
+else
+  return
 fi
 
-if $HAS_RUBY;then
-  # Ruby/Rails
-  alias bi="bundle install"
-  alias bdgem="gem build *.gemspec; gem install *.gem --no-ri --no-rdoc; rbenv rehash"
-  alias rake="noglob rake"
-  alias binstubs="gem regenerate_binstubs"
-  alias coveron="export COVERALLS_RUN_LOCALLY=true"
-  alias coveroff="unset COVERALLS_RUN_LOCALLY"
-fi
+alias bi="bundle install"
+alias bdgem="gem build *.gemspec; gem install *.gem --no-ri --no-rdoc; rbenv rehash"
+alias rake="noglob rake"
+alias binstubs="gem regenerate_binstubs"
+alias coveron="export COVERALLS_RUN_LOCALLY=true"
+alias coveroff="unset COVERALLS_RUN_LOCALLY"
 
