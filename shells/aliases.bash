9e795b0718a77a93697e6c3ffd71fee61da9b17e
Keith Smiley <keithbsmiley@gmail.com>
2013-12-08 20:24:34 -0500

    Update git command alias

--- shells/aliases.bash
+++ shells/aliases.bash
@@ -9,33 +9,20 @@ alias .....="cd ../../../.."
 alias :q="exit"
 
 # Shortcuts
-alias gs="git status -sb"
-alias gss="git status"
-alias Gs="gs"
-alias gc="git clone"
-alias gp="git push"
-alias gd="git diff"
-alias gdt="git difftool"
-alias gdtf="git difftool -t fugitive"
-alias gba="git branch -a"
-alias grv="git remote -v"
-alias gsub="git submodule update --init --recursive"
-gl() {
-    local count=5
-    if [[ $# -gt 0 ]];then
-        local count=$1
-    fi
-
-    local format="format:%Cgreen%h%Creset %s%Cred%d%Creset"
-    if [[ $# -gt 1 ]];then
-        local format="$format %C(black bold)(by %an)%Creset"
-    fi
-
-    git --no-pager log -$count --topo-order --pretty=$format
-}
-
-alias s="subl"
-alias ss="subl ."
+alias g="git"
+alias gs="g s"
+alias Gs="g s"
+alias gp="g p"
+alias gc="g c"
+alias ga="g a"
+alias gap="g ap"
+alias gd="g d"
+alias gdt="g dt"
+alias gdc="g dc"
+alias gba="g ba"
+alias grv="g rv"
+alias gsub="g sub"
+alias gl="g l"
 
 # Vim
 alias vundle="vim +BundleClean +BundleInstall +qall"
@@ -73,9 +60,7 @@ alias la="ls -la ${colorflag}"
 alias ls="command ls ${colorflag}"
 export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
 
-# IP addresses
-alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
-
 # Scripts
 alias t="python $DOTFILES/scripts/t/t.py --task-dir $HOME/Dropbox/tasks --list tasks"
+alias ct="clear; t"
 
