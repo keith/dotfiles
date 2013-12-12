f44aaf6732c7747f886218f9209ec051c71d11a2
Keith Smiley <keithbsmiley@gmail.com>
2013-12-11 20:21:07 -0500

    Update dock hide times

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -94,8 +94,8 @@ defaults write com.apple.dock orientation -string "left"
 defaults write com.apple.dock autohide -bool true
 
 # Change the auto-hiding Dock delay
-defaults write com.apple.dock autohide-delay -float 0.15
-defaults write com.apple.dock autohide-time-modifier -float 0.15
+defaults write com.apple.dock autohide-delay -float 0.05
+defaults write com.apple.dock autohide-time-modifier -float 0.05
 
 # Show indicator lights for open applications in the Dock
 defaults write com.apple.dock show-process-indicators -bool true
