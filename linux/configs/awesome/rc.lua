c02fb3e9713128ff53ca1d445377cd09375269a4
Keith Smiley <keithbsmiley@gmail.com>
2014-01-02 12:25:53 -0500

    Disable sloppy focus, space clock

--- linux/configs/awesome/rc.lua
+++ linux/configs/awesome/rc.lua
@@ -110,7 +110,7 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 
 -- {{{ Wibox
 -- Create a textclock widget
-mytextclock = awful.widget.textclock("%a %b %d, %I %M %p")
+mytextclock = awful.widget.textclock(" %a %b %d, %I %M %p ")
 
 -- Create a wibox for each screen and add it
 mywibox = {}
@@ -367,12 +367,12 @@ awful.rules.rules = {
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function (c, startup)
     -- Enable sloppy focus
-    c:connect_signal("mouse::enter", function(c)
-        if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
-            and awful.client.focus.filter(c) then
-            client.focus = c
-        end
-    end)
+    -- c:connect_signal("mouse::enter", function(c)
+    --     if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
+    --         and awful.client.focus.filter(c) then
+    --         client.focus = c
+    --     end
+    -- end)
 
     if not startup then
         -- Set the windows at the slave,
