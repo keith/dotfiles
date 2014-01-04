eb6565745b07cdcb60127da41e3a9a5f8c3f755e
Keith Smiley <keithbsmiley@gmail.com>
2014-01-04 00:12:20 -0500

    Fix offlineimap helpers

--- mutt/offlineimaphelpers.py
+++ mutt/offlineimaphelpers.py
@@ -15,14 +15,17 @@ def get_keychain_pass(account=None, server=None):
         return get_env()
 
     home = os.environ.get('HOME')
+    user = os.environ.get('USER')
+
     params = {
         'security': '/usr/bin/security',
         'command': 'find-internet-password',
         'account': account,
         'server': server,
         'keychain': home + '/Library/Keychains/login.keychain',
+        'user': user,
     }
-    command = "sudo -u ksmiley %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    command = "sudo -u %(user)s %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
     output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
     outtext = [l for l in output.splitlines()
                if l.startswith('password: ')][0]
