f613ebcc43095ce2eb922c57c8caa3869b831a1e
Keith Smiley <keithbsmiley@gmail.com>
2014-02-04 22:26:41 -0500

    Remove old scripts

--- shells/scripts.bash
+++ shells/scripts.bash
@@ -1,13 +1,8 @@
 # Path to script files in dotfiles directory
-export SCRIPT_PATH="$DOTFILES/scripts"
+SCRIPT_PATH="$DOTFILES/scripts"
 
 # Scripts
-alias append="$SCRIPT_PATH/fileAppend.rb"
 alias cloudfront="$SCRIPT_PATH/cloudFront.rb"
-alias methods="$SCRIPT_PATH/methods.rb"
-alias mksite="$SCRIPT_PATH/newSite.rb"
-alias pf="$SCRIPT_PATH/processfile.rb"
-alias rename="$SCRIPT_PATH/imageRenamer.rb"
 
 export _Z_EXCLUDE_DIRS="/Volumes"
 source "$SCRIPT_PATH/z/z.sh"
