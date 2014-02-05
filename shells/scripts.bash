7313e13133c8482024b6fba2309be0d60d9522bb
Keith Smiley <keithbsmiley@gmail.com>
2014-02-04 22:05:31 -0500

    Move lc script

--- shells/scripts.bash
+++ shells/scripts.bash
@@ -4,13 +4,10 @@ export SCRIPT_PATH="$DOTFILES/scripts"
 # Scripts
 alias append="$SCRIPT_PATH/fileAppend.rb"
 alias cloudfront="$SCRIPT_PATH/cloudFront.rb"
-alias lc="$SCRIPT_PATH/license.rb"
 alias methods="$SCRIPT_PATH/methods.rb"
 alias mksite="$SCRIPT_PATH/newSite.rb"
 alias pf="$SCRIPT_PATH/processfile.rb"
 alias rename="$SCRIPT_PATH/imageRenamer.rb"
 
-if [[ -e "$SCRIPT_PATH/z/z.sh" ]];then
-    export _Z_EXCLUDE_DIRS="/Volumes"
-    source "$SCRIPT_PATH/z/z.sh"
-fi
+export _Z_EXCLUDE_DIRS="/Volumes"
+source "$SCRIPT_PATH/z/z.sh"
