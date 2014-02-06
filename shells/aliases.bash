811a041dfc3ff4a63b3f78458ec108585d0c21e8
Keith Smiley <keithbsmiley@gmail.com>
2014-02-06 11:03:24 -0500

    Remove old alias

--- shells/aliases.bash
+++ shells/aliases.bash
@@ -46,5 +46,4 @@ export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40
 
 # Scripts
 alias todo="python $DOTFILES/scripts/t/t.py --task-dir $HOME/Dropbox/tasks --list tasks"
-alias ct="clear; t"
 
