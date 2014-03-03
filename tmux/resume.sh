b1b78a5bb558e956513d18505bb439aa7af27153
Keith Smiley <keithbsmiley@gmail.com>
2014-03-02 22:50:35 -0500

    Remove tmux resume echo

--- tmux/resume.sh
+++ tmux/resume.sh
@@ -1,6 +1,5 @@
 index=$(tmux list-panes | grep -oE '\d+\s\(active\)' | awk '{ print $1; }')
 __pwd=$(tmux show-environment | grep -e "^TMUXPWD_$index=" | sed 's/.*=//')
-echo "$active | $index | $__pwd"
 if [[ -z $__pwd ]];then
     __pwd=$HOME
 fi
