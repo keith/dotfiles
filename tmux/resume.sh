1867febd8a669a7034e85ee79112901fd21b9da4
Keith Smiley <keithbsmiley@gmail.com>
2014-04-06 16:45:32 -0400

    Ignore directories that don't exist

--- tmux/resume.sh
+++ tmux/resume.sh
@@ -1,6 +1,6 @@
 index=$(tmux list-panes | grep -oE '\d+\s\(active\)' | awk '{ print $1; }')
 __pwd=$(tmux show-environment | grep -e "^TMUXPWD_$index=" | sed 's/.*=//')
-if [[ -z $__pwd ]];then
+if [[ -z "$__pwd" || ! -d "$__pwd" ]];then
     __pwd=$HOME
 fi
 tmux $1 $2 -c "$__pwd"
