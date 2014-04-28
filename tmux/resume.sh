1b735c232cfb410e9761e2dda31b8b0fa1abb2f2
Keith Smiley <keithbsmiley@gmail.com>
2014-04-28 16:22:56 -0400

    Update tmux resume with waster functions
    https://github.com/erikw/tmux-powerline/blob/6c35c956d2e96418066ad7218983892ba59926ab/lib/tmux_adapter.sh

--- tmux/resume.sh
+++ tmux/resume.sh
@@ -1,5 +1,5 @@
-index=$(tmux list-panes | grep -oE '\d+\s\(active\)' | awk '{ print $1; }')
-__pwd=$(tmux show-environment | grep -e "^TMUXPWD_$index=" | sed 's/.*=//')
+index=$(tmux display -p "TMUXPWD_#D" | tr -d %)
+__pwd=$(tmux show-environment | grep --color=never "$index" | sed 's/^.*=//')
 if [[ -z "$__pwd" || ! -d "$__pwd" ]];then
     __pwd=$HOME
 fi
