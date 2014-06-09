d2ad5e2d48666d1e3bc79cfc5043debb9e39d072
Keith Smiley <keithbsmiley@gmail.com>
2014-06-08 18:52:34 -0700

    Last failed attempt at a /// snippet for ObjC

--- vim/pythonx/snippet_helpers.py
+++ vim/pythonx/snippet_helpers.py
@@ -4,12 +4,18 @@ import vim
 
 def objc_comment_block():
     line_num = current_line_number() + 1
-    method = line_at_number(line_num)
-    r = re.compile(":\s*\(\w+\s*\*?\)\s*(\w+)")
-    while "{" not in method:
-        line_num += 1
-        method += " " + line_at_number(line_num)
+    lines = line_in_range(line_num, line_num + 5)
+    method = lines[0]
+    return lines
+    if not (method.startswith("-") or method.startswith("+")):
+        return ""
 
+    while "{" not in method and line_num < len(lines):
+        line_num += 1
+        print "getting line " + line_num
+        method += " " + lines[line_num]
+
+    r = re.compile(":\s*\(\w+\s*\*?\)\s*(\w+)")
     variables = r.findall(method)
     return comment_block(variables_string(variables))
 
@@ -31,5 +37,5 @@ def current_line_number():
     return int(vim.eval("line('.')"))
 
 
-def line_at_number(num):
-    return vim.eval("getline('%s')" % num)
+def line_in_range(start, end):
+    return vim.eval("getline(%d, %d)" % (start, end))
