867351b8cf7b4e4ff3b30733d9c81722cf755c20
Keith Smiley <keithbsmiley@gmail.com>
2014-06-08 20:01:01 -0700

    The monthly fish attempt

--- linux/configs/fish/config.fish
+++ linux/configs/fish/config.fish
@@ -1,2 +1,12 @@
 set fish_greeting
 set -gx PATH /usr/local/bin $PATH
+
+set fish_key_bindings fish_vi_key_bindings
+
+function fish_right_prompt
+  if [ "$fish_bind_mode" = "default" ]
+    printf "[NORMAL]"
+  end
+end
+
+# TODO: Readline style CTRL-a/e/w
