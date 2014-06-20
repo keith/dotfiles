9839556bf4845ea35359af982dc47e12d7b29422
Keith Smiley <keithbsmiley@gmail.com>
2014-06-20 10:50:00 -0700

    Haskell install script

--- langs/haskell.sh
+++ langs/haskell.sh
@@ -4,11 +4,21 @@ set -e
 
 ghc_version="7.8.2"
 filename="ghc.tar.xz"
+cabal_version="1.20.0.2"
+cabal_filename="cabal.tar.gz"
 
 wget "http://www.haskell.org/ghc/dist/$ghc_version/ghc-$ghc_version-x86_64-apple-darwin-mavericks.tar.xz" -O "$filename"
 
-tar xf $filename
-cd ghc-$version
+tar xf "$filename"
+pushd "ghc-$ghc_version"
 ./configure
+make install
+popd
+
+wget "http://hackage.haskell.org/package/cabal-install-$cabal_version/cabal-install-$cabal_version.tar.gz" -O "$cabal_filename"
+tar -zxvf "$cabal_filename"
+pushd "cabal-install-$cabal_version"
+./bootstrap.sh --no-doc
+popd
 
 # vim: tw=0
