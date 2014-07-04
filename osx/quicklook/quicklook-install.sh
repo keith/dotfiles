170ff568f168235003fcfb53cd9022b99c77a0ae
Keith Smiley <keithbsmiley@gmail.com>
2014-07-04 12:04:49 -0700

    Update quicklook install script for ghb

--- osx/quicklook/quicklook-install.sh
+++ osx/quicklook/quicklook-install.sh
@@ -3,22 +3,22 @@
 qldirectory=~/Library/QuickLook
 
 PLUGINS=( \
-    https://github.com/whomwah/qlstephen/releases/download/1.4.2/QLStephen.qlgenerator.1.4.2.zip \
-    https://github.com/Keithbsmiley/PlaygroundQuickLook/releases/download/0.1.0/PlaygroundQuickLook.qlgenerator.zip \
+  whomwah/qlstephen \
+  Keithbsmiley/PlaygroundQuickLook \
 )
 
 mkdir -p "$qldirectory"
 for PLUGIN in ${PLUGINS[@]}
 do
-    wget "$PLUGIN"
+  filename=$(ghb download-release -f -r $PLUGIN)
+  extract "$filename"
 done
 
-unzip \*.zip
 qlgenerators=(*.qlgenerator)
 for qlgenerator in ${qlgenerators[@]}
 do
-    rm -rf "$qldirectory/$qlgenerator"
-    mv "$qlgenerator" "$qldirectory"
+  rm -rf "$qldirectory/$qlgenerator"
+  mv "$qlgenerator" "$qldirectory"
 done
 
-rm *.zip
+rm release.*
