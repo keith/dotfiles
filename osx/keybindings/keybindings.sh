48ffdc9369e2c0fc7a10b526d54644831a6a42ca
Keith Smiley <keithbsmiley@gmail.com>
2014-07-05 19:24:50 -0700

    Add OS X keybindings folder for ^w

---
+++ osx/keybindings/keybindings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+linkdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+keybinddir=~/Library/KeyBindings
+mkdir -p $keybinddir
+ln -s $linkdir/DefaultKeyBinding.dict $keybinddir
