9da0b89a2be5f7880d44e23ba4b19bac5e1011f5
Keith Smiley <keithbsmiley@gmail.com>
2014-07-07 13:50:11 -0700

    Case insensitive inside word zsh completion

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -1,5 +1,6 @@
 # matches case insensitive
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
+  'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
