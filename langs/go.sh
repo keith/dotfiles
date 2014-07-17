acaa4fc9bec3e51dc8ae3575e01270d2d93c4453
Keith Smiley <keithbsmiley@gmail.com>
2014-07-16 21:54:12 -0700

    Source ruby setup instead of nesting shells

--- langs/go.sh
+++ langs/go.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
 if ! which go &> /dev/null; then
-    echo "You must install Go (or add it to your \$PATH) before install modules"
-    exit
+  echo "You must install Go (or add it to your \$PATH) before install modules"
+  exit
 fi
 
 function install () {
-    go get -u github.com/jstemmer/gotags
+  go get -u github.com/jstemmer/gotags
 }
 
 function die () {
-    echo "Usage ./$(basename $0) install"
-    exit
+  echo "Usage ./$(basename $0) install"
+  exit
 }
 
 if [[ $# != 1 ]];then
-    die
+  die
 fi
 
 if [[ $1 == "install" ]];then
-    install
+  install
 else
-    die
+  die
 fi
-
--- langs/rbenv.sh
+++ langs/rbenv.sh
@@ -3,36 +3,36 @@
 rbenvpath="$HOME/.rbenv"
 
 function install () {
-    if [[ -d $rbenvpath ]];then
-        echo "$HOME/.rbenv already exists"
-        exit
-    fi
+  if [[ -d $rbenvpath ]];then
+    echo "$HOME/.rbenv already exists"
+    exit
+  fi
 
-    git clone https://github.com/sstephenson/rbenv.git $rbenvpath
-    git clone https://github.com/sstephenson/rbenv-default-gems.git $rbenvpath/plugins/rbenv-default-gems
-    git clone https://github.com/sstephenson/ruby-build.git $rbenvpath/plugins/ruby-build
-    git clone https://github.com/sstephenson/rbenv-gem-rehash.git $rbenvpath/plugins/rbenv-gem-rehash
-    git clone https://github.com/tpope/rbenv-ctags.git $rbenvpath/plugins/rbenv-ctags
-    git clone https://github.com/rkh/rbenv-update.git $rbenvpath/plugins/rbenv-update
+  git clone https://github.com/sstephenson/rbenv.git $rbenvpath
+  git clone https://github.com/sstephenson/rbenv-default-gems.git $rbenvpath/plugins/rbenv-default-gems
+  git clone https://github.com/sstephenson/ruby-build.git $rbenvpath/plugins/ruby-build
+  git clone https://github.com/sstephenson/rbenv-gem-rehash.git $rbenvpath/plugins/rbenv-gem-rehash
+  git clone https://github.com/tpope/rbenv-ctags.git $rbenvpath/plugins/rbenv-ctags
+  git clone https://github.com/rkh/rbenv-update.git $rbenvpath/plugins/rbenv-update
 
-    ln -s $DOTFILES/default-gems $rbenvpath
+  ln -s $DOTFILES/default-gems $rbenvpath
 
-    exec $SHELL -l
+  source $DOTFILES/langs/ruby.bash
 }
 
 function die () {
-    echo "Usage ./$(basename $0) {install|remove}"
-    exit
+  echo "Usage ./$(basename $0) {install|remove}"
+  exit
 }
 
 if [[ $# != 1 ]]; then
-    die
+  die
 fi
 
 if [[ $1 == "install" ]]; then
-    install
+  install
 elif [[ $1 == "remove" ]]; then
-    rm -rf $rbenvpath
+  rm -rf $rbenvpath
 else
-    die
+  die
 fi
