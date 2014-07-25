ef042cdd304f5a80d060c251671e5e1683920a14
Keith Smiley <keithbsmiley@gmail.com>
2014-07-25 09:19:22 -0700

    Update the update config

--- hydra/menuconfig.lua
+++ hydra/menuconfig.lua
@@ -2,15 +2,11 @@ dofile(package.searchpath("updateconfig", package.path))
 
 -- show a helpful menu
 hydra.menu.show(function()
-    local updatetitles = {[true] = "Install Update", [false] = "Check for Update..."}
-    local updatefns = {[true] = hydra.updates.install, [false] = checkforupdates}
-    local hasupdate = (hydra.updates.newversion ~= nil)
-
-    return {
-      {title = "Reload Config", fn = hydra.reload},
-      {title = "-"},
-      {title = "About", fn = hydra.showabout},
-      {title = updatetitles[hasupdate], fn = updatefns[hasupdate]},
-      {title = "Quit Hydra", fn = os.exit},
-    }
+  return {
+    {title = "Reload Config", fn = hydra.reload},
+    {title = "-"},
+    {title = "About", fn = hydra.showabout},
+    {title = "Check for Updates...", fn = function() hydra.updates.check(nil, true) end},
+    {title = "Quit", fn = os.exit},
+  }
 end)
