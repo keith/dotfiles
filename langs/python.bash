7d722250e041d43ff598a77e10334b9c4f5b69c5
Keith Smiley <keithbsmiley@gmail.com>
2014-07-28 13:57:29 -0700

    Fix spacing

--- langs/python.bash
+++ langs/python.bash
@@ -3,6 +3,6 @@ export PYTHONSTARTUP=$DOTFILES/langs/pystartup
 
 export PYENV_ROOT="$HOME/.pyenv"
 if [[ -d "$PYENV_ROOT" ]]; then
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
 fi
