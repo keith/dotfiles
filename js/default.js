ee9bed9fee6d8f0b34a06efe0bacec6902736600
Keith Smiley <keithbsmiley@gmail.com>
2014-08-17 14:41:46 -0700

    Fucking Quora

--- js/default.js
+++ js/default.js
@@ -1,3 +1,7 @@
+String.prototype.contains = function(str) {
+  return this.indexOf(str) > -1;
+};
+
 String.prototype.endsWith = function(suffix) {
   return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
---
+++ js/quora.com.js
@@ -0,0 +1,16 @@
+function appendParameter(param) {
+  var URL = window.location.href;
+  if (URL.contains("?")) {
+    window.location.href += "&" + param;
+  } else {
+    window.location.href = "?" + param;
+  }
+}
+
+$(document).ready(function() {
+  var URL = window.location.href;
+  var param = "share=1"
+  if (!URL.contains(param)) {
+    appendParameter(param);
+  }
+});
