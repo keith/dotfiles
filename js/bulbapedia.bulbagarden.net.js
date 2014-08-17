239f7c8b694203f7e5c6ab204e6667f4496bc13d
Keith Smiley <keithbsmiley@gmail.com>
2014-08-17 09:43:25 -0700

    Nothing to see here

---
+++ js/bulbapedia.bulbagarden.net.js
@@ -0,0 +1,21 @@
+function jump(anchor) {
+  var url = location.href;
+  location.href = "#" + anchor
+}
+
+$(document).keypress(function(e) {
+  switch (e.which) {
+    // e
+    case 101:
+      jump("Evolution");
+      break;
+    // t
+    case 116:
+      jump("Type_effectiveness");
+      break;
+    // w
+    case 119:
+      jump("Game_locations");
+      break;
+  }
+});
