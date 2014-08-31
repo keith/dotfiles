8902a052be67c75a1b734fbbb0ded4fdc9af9eea
Keith Smiley <keithbsmiley@gmail.com>
2014-08-31 01:07:33 -0700

    GUI haskell-platform paths

--- langs/haskell.bash
+++ langs/haskell.bash
@@ -1,6 +1,10 @@
 # Check that cabal has been setup
-if [[ -d "$HOME/.cabal" ]];then
+if [[ -d "$HOME/.cabal" ]]; then
   # Add cabal installed packages to PATH
   PATH="$HOME/.cabal/bin:$PATH"
 fi
 
+# If haskell is installed through GUI haskell-platform
+if [[ -d "$HOME/Library/Haskell/bin" ]]; then
+  PATH="$HOME/Library/Haskell/bin:$PATH"
+fi
