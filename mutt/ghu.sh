e548af7b1418ea2f7f8159bd13bc7528c908f71e
Keith Smiley <keithbsmiley@gmail.com>
2014-09-12 10:21:11 -0700

    Move ghu inline

---
+++ mutt/ghu.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+# pbrisbin 2014
+# https://github.com/pbrisbin/ghu.git
+#
+###
+share="$(dirname "$0")"/../share/ghu
+unsubscribe=$(sed "/^List-Unsubscribe: /,/^[A-Z].*: /!d
+  #
+  # Result:
+  #
+  #   List-Unsubscribe: <mailto:...>,
+  #    <https://...>
+  #   X-Whatever: ...
+  #
+
+s/^[A-Z].*: //g
+  #
+  # Result:
+  #
+  #   <mailto:...>,
+  #    <https://...>
+  #   ...
+  #
+
+/http/!d
+  #
+  # Result:
+  #
+  #    <https://...>
+  #
+
+s/^ *<\([^>]*\)> *$/\1/g
+  #
+  # Result:
+  #
+  #   https:...
+  #
+")
+
+[ -n "$unsubscribe" ] || exit 1
+
+curl -# "$unsubscribe" >/dev/null && printf "OK\n"
