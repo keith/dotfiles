71d690bd4a08ad416a4ac3b594f8f9510e8f8843
Keith Smiley <keithbsmiley@gmail.com>
2014-10-07 10:01:02 -0700

    Set max timeout

--- mutt/ghnotification.sh
+++ mutt/ghnotification.sh
@@ -2,4 +2,4 @@
 
 beacon_re='https://github\.com/notifications/beacon/.*\.gif'
 beacon_url="$(cat | grep -o "$beacon_re")"
-[ -n "$beacon_url" ] && curl --silent "$beacon_url" >/dev/null
+[ -n "$beacon_url" ] && curl --silent --max-time 3 "$beacon_url" >/dev/null
