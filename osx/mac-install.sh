e0d5ff6c9ada083e6a5a32c6036516a262a8567e
Keith Smiley <keithbsmiley@gmail.com>
2014-10-19 12:02:03 -0700

    Update homebrew install

--- osx/mac-install.sh
+++ osx/mac-install.sh
@@ -4,8 +4,8 @@
 set -e
 
 if [[ ! -e "../manage.sh" ]]; then
-    echo "Make sure you have the manage script nearby"
-    exit
+  echo "Make sure you have the manage script nearby"
+  exit
 fi
 
 cd $(dirname $0)
@@ -13,15 +13,15 @@ cd ..
 ./manage.sh install
 
 if [[ ! -e "$HOME/.bashrc" ]]; then
-    echo "Looks like the manage script failed, try and run it manually"
-    exit
+  echo "Looks like the manage script failed, try and run it manually"
+  exit
 fi
 
 source "$HOME/.bashrc"
 
 if ! which brew &> /dev/null; then
-    echo "Installing homebrew"
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  echo "Installing homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew bundle $DOTFILES/osx/Brewfile
