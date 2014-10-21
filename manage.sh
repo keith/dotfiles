c4c6f47828dabb901a14e693d1c3ef55606b4e2a
Keith Smiley <keithbsmiley@gmail.com>
2014-10-21 16:42:54 -0700

    Remove unused install script stuff

--- manage.sh
+++ manage.sh
@@ -45,7 +45,6 @@ FILES=(\
     zshrc \
 )
 LINUX=(Xdefaults Xmodmap)
-NO_DOT=()
 
 function custom_path () {
     for i in "${!PATHS[@]}"
@@ -58,26 +57,8 @@ function custom_path () {
     return 1
 }
 
-function no_dot () {
-    local filename=$1
-    for FILE in ${NO_DOT[@]}
-    do
-        if [[ $FILE == $filename ]]; then
-            return 0
-        fi
-    done
-
-    return 1
-}
-
 function new_path () {
-    local filename=$1
-
-    if ! no_dot $filename; then
-        filename=".$filename"
-    fi
-
-    echo "$HOME/$filename"
+    echo "$HOME/.$1"
 }
 
 # Links the passed filename to its new location
