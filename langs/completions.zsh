f211f72c06846e7b1048024d5ab70d8eccc826b4
Keith Smiley <keithbsmiley@gmail.com>
2014-11-02 12:19:28 -0800

    Remove unused golang completions

--- langs/completions.zsh
+++
@@ -1,9 +0,0 @@
-# Completions for language tools
-completions=($GOROOT/share/zsh/site-functions/go $GOROOT/misc/zsh/go)
-for completion in ${completions}
-do
-  if [[ -e $completion ]];then
-    source $completion
-  fi
-done
-
