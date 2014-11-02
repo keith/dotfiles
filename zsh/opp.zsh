039b44f097276d765087cd2cf50bb83d2284b8b6
Keith Smiley <keithbsmiley@gmail.com>
2014-11-02 12:19:40 -0800

    Rename configs -> opp_configs
    This configs array conflicts with the configs file in ~/.zshrc and the
    like. This fixes that.

--- zsh/opp.zsh
+++ zsh/opp.zsh
@@ -1,9 +1,11 @@
-typeset -U configs
-configs=($DOTFILES/scripts/opp/*.zsh $DOTFILES/scripts/opp/*/*.zsh)
+typeset -U opp_configs
+opp_configs=($DOTFILES/scripts/opp/*.zsh $DOTFILES/scripts/opp/*/*.zsh)
 
-for file in ${configs:#*/test*.zsh}
+for file in ${opp_configs:#*/test*.zsh}
 do
   source $file
 done
 
 bindkey -M vicmd '~' vi-swap-case
+
+unset opp_configs
