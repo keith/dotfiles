d4f84f99e9a1b6046f4b1b401707948f8ec2356d
Keith Smiley <keithbsmiley@gmail.com>
2014-11-02 11:14:14 -0800

    Remove unused script

--- bash/scripts.bash
+++ bash/scripts.bash
@@ -1,8 +1,2 @@
-# Path to script files in dotfiles directory
-SCRIPT_PATH="$DOTFILES/scripts"
-
-# Scripts
-alias cloudfront="$SCRIPT_PATH/cloudFront.rb"
-
 export _Z_EXCLUDE_DIRS="/Volumes"
-source "$SCRIPT_PATH/z/z.sh"
+source "$DOTFILES/scripts/z/z.sh"
