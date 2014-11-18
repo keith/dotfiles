119c0667e9b8c15007f04aa4fc115370fa5e8fd3
Keith Smiley <keithbsmiley@gmail.com>
2014-11-17 16:21:09 -0800

    Remove all pyenv related junk

--- langs/python.bash
+++ langs/python.bash
@@ -1,9 +1,2 @@
 export PYTHONPATH=".:/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
 export PYTHONSTARTUP=$DOTFILES/langs/pystartup
-
-PYENV_ROOT="$HOME/.pyenv"
-if [[ -d "$PYENV_ROOT" ]]; then
-  export PYENV_ROOT
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init -)"
-fi
