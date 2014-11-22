bd7ecd56a876c083c6fa068706c52b1eca11a022
Keith Smiley <keithbsmiley@gmail.com>
2014-11-22 11:02:01 -0800

    Delete derived data alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -44,3 +44,6 @@ export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40
 
 # Remove all items from the dock
 alias cleardock="defaults write com.apple.dock persistent-apps -array \"\""
+
+# Clear the derived data folder
+alias ddd='rm -rf ~/Library/Developer/Xcode/DerivedData/*'
