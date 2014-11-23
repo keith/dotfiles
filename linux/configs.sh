a409fe3806a3b627c0d1cdb69f243008fd5d821b
Keith Smiley <keithbsmiley@gmail.com>
2014-11-22 22:36:24 -0800

    Spacing

--- linux/configs.sh
+++ linux/configs.sh
@@ -5,34 +5,33 @@ FILES=$(ls $dir)
 p=$HOME/.config
 
 function link() {
-    local folder=$1
+  local folder=$1
 
-    if [[ ! -d $dir/$folder ]];then
-        echo "$folder doesn't exist"
-        return
-    fi
+  if [[ ! -d $dir/$folder ]];then
+    echo "$folder doesn't exist"
+    return
+  fi
 
-    local path=$p
-    if [[ ! -d $path/$folder ]];then
-        echo "Linking $dir/$folder to $path"
-        ln -s $dir/$folder $path
-    fi
+  local path=$p
+  if [[ ! -d $path/$folder ]];then
+    echo "Linking $dir/$folder to $path"
+    ln -s $dir/$folder $path
+  fi
 }
 
 function new_path() {
-    local folder=$1
-    echo "$p/$folder"
-}
+  local folder=$1
+  echo "$p/$folder"
+  }
 
-function install_links() {
-    for FILE in ${FILES[@]};do
-        link $FILE
-    done
+  function install_links() {
+  for FILE in ${FILES[@]};do
+    link $FILE
+  done
 }
 
 if [[ ! -d "$p" ]];then
-    mkdir -p $p
+  mkdir -p $p
 fi
 
 install_links
-
