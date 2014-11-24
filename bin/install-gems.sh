88c01b6ed3c176068755891f8cb9f4f80a39654a
Keith Smiley <keithbsmiley@gmail.com>
2014-11-24 01:16:17 -0800

    Move gem install script to bash

---
+++ bin/install-gems.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+trap ctrl_c INT
+function ctrl_c() {
+  exit 130
+}
+
+gems=$(cat ~/.rbenv/default-gems)
+for gem in ${gems[@]}
+do
+  gem install $gem
+done
--- install-gems.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-$FILE = 'default-gems'
-contents = File.read $FILE
-gems = contents.split(/\n/)
-
-gems.each do |gem|
-  print %x[gem install "#{ gem }"]
-end
