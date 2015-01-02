0e42d046585290a5d88120c75626d9bfd0f2dbec
John Hiott <jhiott@oolcay.com>
2015-01-02 10:48:47 -0500

    fix error that prevents binary files from being hidden

--- js/github.com.js
+++ js/github.com.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     var link = $("<a href=\"#\" class=\"minibutton\">Hide</a>");
     $(this).prepend(link);
     link.click(function() {
-      var codeArea = $(diff).find(".blob-wrapper");
+      var codeArea = $(diff).find('[class$="wrapper"]');
       $(this).html() == 'Hide' ? codeArea.hide() : codeArea.show()
       $(this).html($(this).html() == 'Show' ? 'Hide' : 'Show');
       return false;
