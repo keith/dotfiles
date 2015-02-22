fdb3b1e56edeaddb5c1590832af39280088136a5
Keith Smiley <keithbsmiley@gmail.com>
2015-02-22 13:48:10 -0600

    Move folder filters and translations to python

--- mutt/offlineimaphelpers.py
+++ mutt/offlineimaphelpers.py
@@ -26,9 +26,46 @@ def get_keychain_pass(account=None, server=None):
         'user': user,
     }
     command = "sudo -u %(user)s %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
-    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    output = subprocess.check_output(command, shell=True,
+                                     stderr=subprocess.STDOUT)
     outtext = [l for l in output.splitlines()
                if l.startswith('password: ')][0]
 
     return re.match(r'password: "(.*)"', outtext).group(1)
 
+
+def local_nametrans(folder):
+    return {
+        'archive': '[Gmail]/All Mail',
+        'drafts':  '[Gmail]/Drafts',
+        'sent':    '[Gmail]/Sent Mail',
+        'trash':   '[Gmail]/Trash',
+    }.get(folder, folder)
+
+
+def remote_nametrans(folder):
+    return {
+        '[Gmail]/All Mail':  'archive',
+        '[Gmail]/Drafts':    'drafts',
+        '[Gmail]/Sent Mail': 'sent',
+        '[Gmail]/Trash':     'trash',
+    }.get(folder, folder)
+
+
+def folder_filter(folder):
+    return folder not in {
+        '[Airmail]',
+        '[Airmail]/Done',
+        '[Airmail]/Memo',
+        '[Airmail]/To Do',
+        '[Gmail]/Important',
+        '[Gmail]/Spam',
+        '[Gmail]/Starred',
+        '[Mailbox]',
+        '[Mailbox]/Later',
+        '[Mailbox]/To Buy',
+        '[Mailbox]/To Read',
+        '[Mailbox]/To Watch',
+        'flagged',
+        'Notes',
+    }
