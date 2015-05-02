503dcbd9a9aba4d1b97f01d3e40c641a222c39c9
Keith Smiley <keithbsmiley@gmail.com>
2015-05-02 12:42:54 -0700

    Remove selecta zsh command

--- zsh/selecta.zsh
+++
@@ -1,7 +0,0 @@
-# CTRL-R - Paste the selected command from history into the command line
-history-widget() {
-  LBUFFER=$(fc -l 1 | sed "s/ *[0-9*]* *//" | selecta)
-  zle redisplay
-}
-zle     -N   history-widget
-bindkey '^S' history-widget
