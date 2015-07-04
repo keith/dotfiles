1152f35737f47f5b84d5d9c23856526fe2409469
Keith Smiley <keithbsmiley@gmail.com>
2015-07-03 20:44:18 -0700

    Update scripts syntax

--- bin/install-gems.sh
+++ bin/install-gems.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 trap ctrl_c INT
-function ctrl_c() {
+ctrl_c() {
   exit 130
 }
 
 gems=$(cat ~/.rbenv/default-gems)
-for gem in "${gems[@]}"
+for gem in $gems
 do
   gem install "$gem"
 done
