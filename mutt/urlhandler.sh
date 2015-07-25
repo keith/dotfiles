fea352cfbf274921bfaf18eeda9b5c40db7ef747
Keith Smiley <keithbsmiley@gmail.com>
2015-07-25 10:05:35 -0700

    Fuck you google chrome
    This fixes a bug/feature of Google Chrome. In a recent (silent) update,
    Chrome now brings itself to the foreground whenever opening a new tab,
    even when the `-g` argument is passed to `open`. This script now checks
    the foreground app before and after, if they differ, open the original.
    
    If only Safari was usable!

--- mutt/urlhandler.sh
+++ mutt/urlhandler.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
+# Because Chrome
+frontmost() {
+osascript <<EOF
+tell application "System Events"
+  set frontApp to name of first application process whose frontmost is true
+end tell
+EOF
+}
+
+before=$(frontmost)
 open -g "$1"
+after=$(frontmost)
+
+if [ "$before" != "$after" ]; then
+  open -a "$before"
+fi
