fa2cfdacb057efd0a1b1a298280f520806a1bd27
Keith Smiley <keithbsmiley@gmail.com>
2015-08-24 00:30:24 -0700

    chruby config

--- langs/ruby.bash
+++ langs/ruby.bash
@@ -1,10 +1,14 @@
+CHRUBY="/usr/local/opt/chruby"
 RBENVPATH="$HOME/.rbenv"
 RVMPATH="$HOME/.rvm"
 
-if [[ -d $RBENVPATH ]];then
+if [[ -d $CHRUBY ]]; then
+  source $CHRUBY/share/chruby/chruby.sh
+  chruby "$(chruby | sort | tail -1 | tr -d " *")"
+elif [[ -d $RBENVPATH ]]; then
   PATH="$RBENVPATH/bin:$PATH"
   eval "$(rbenv init - --no-rehash)"
-elif [[ -d $RVMPATH ]];then
+elif [[ -d $RVMPATH ]]; then
   PATH="$RVMPATH/bin:$PATH"
 else
   return
