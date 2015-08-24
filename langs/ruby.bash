8af9535fc591598f9a105df2f2f2299963f43f57
Keith Smiley <keithbsmiley@gmail.com>
2015-08-24 00:12:45 -0700

    Remove unnecessary aliases

--- langs/ruby.bash
+++ langs/ruby.bash
@@ -2,16 +2,13 @@ RBENVPATH="$HOME/.rbenv"
 RVMPATH="$HOME/.rvm"
 
 if [[ -d $RBENVPATH ]];then
-    PATH="$RBENVPATH/bin:$PATH"
-    eval "$(rbenv init - --no-rehash)"
+  PATH="$RBENVPATH/bin:$PATH"
+  eval "$(rbenv init - --no-rehash)"
 elif [[ -d $RVMPATH ]];then
-    PATH="$RVMPATH/bin:$PATH"
+  PATH="$RVMPATH/bin:$PATH"
 else
-    return
+  return
 fi
 
 alias bi="bundle install"
-alias bdgem="gem build *.gemspec; gem install *.gem --no-ri --no-rdoc; rbenv rehash"
-alias binstubs="gem regenerate_binstubs"
-alias coveron="export COVERALLS_RUN_LOCALLY=true"
-alias coveroff="unset COVERALLS_RUN_LOCALLY"
+alias bdgem="gem build *.gemspec; gem install *.gem --no-ri --no-rdoc"
