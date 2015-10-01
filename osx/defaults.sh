6e44872311585f3b5ac47f9afccc12040931936b
Keith Smiley <keithbsmiley@gmail.com>
2015-10-01 09:11:45 -0700

    Ignore missing sleepimage for subsequent runs

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -248,7 +248,7 @@ sudo pmset -a sms 0
 # Disable Sleep image
 sudo pmset hibernatemode 0
 # Remove the sleep image file to save disk space
-sudo rm /private/var/vm/sleepimage
+sudo rm -f /private/var/vm/sleepimage
 # Create a zero-byte file instead…
 sudo touch /private/var/vm/sleepimage
 # and make sure it can’t be rewritten
