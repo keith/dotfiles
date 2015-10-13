9d782c75ad65319edafe3723be5a4e798361a3ab
Keith Smiley <keithbsmiley@gmail.com>
2015-10-12 23:17:42 -0700

    Switch to urxvt

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -42,7 +42,7 @@ end
 beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "terminator"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
