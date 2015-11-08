80c1fdff9bede8292d4eab63d2e8109758afbfb1
Keith Smiley <keithbsmiley@gmail.com>
2015-11-07 21:14:48 -0800

    Who needs ruby version managers?

--- langs/ruby.bash
+++ langs/ruby.bash
@@ -1,17 +1,4 @@
-CHRUBY="/usr/local/opt/chruby"
-RBENVPATH="$HOME/.rbenv"
-RVMPATH="$HOME/.rvm"
-
-if [[ -d $CHRUBY ]]; then
-  source $CHRUBY/share/chruby/chruby.sh
-  chruby "$(chruby | sort | tail -1 | tr -d " *")"
-elif [[ -d $RBENVPATH ]]; then
-  PATH="$RBENVPATH/bin:$PATH"
-  eval "$(rbenv init - --no-rehash)"
-elif [[ -d $RVMPATH ]]; then
-  PATH="$RVMPATH/bin:$PATH"
-else
-  return
-fi
+export GEM_HOME=$HOME/.gem
+export PATH=$GEM_HOME/bin:$PATH
 
 alias bi="bundle install"
