349c7df4454dfc717b0edd7503a12a7e0b4c81d8
Keith Smiley <keithbsmiley@gmail.com>
2015-11-07 21:19:52 -0800

    Remove unused go config

--- langs/go.bash
+++ langs/go.bash
@@ -3,23 +3,6 @@ export GOPATH=$HOME/gophers
 # Add Go projects bin to PATH
 PATH="$GOPATH/bin:$PATH"
 
-# Go version manager configs, for the future
-# goenv=$HOME/.goenv
-# if [[ -d $goenv ]]; then
-#     export PATH="$goenv/bin:$PATH"
-#     eval "$(goenv init -)"
-#     unset goenv
-#     return
-# fi
-
-# export GVM_ROOT=$HOME/.gvm
-# if [[ -d $GVM_ROOT ]]; then
-#     source $GVM_ROOT/scripts/gvm-default
-#     return
-# else
-#     unset GVM_ROOT
-# fi
-
 if ! which go &> /dev/null; then
   if [[ "$OSX" == true ]]; then
     go_root=$(brew --prefix go)
