74d3e871f97738e7e70cbd24f17226c7cb11339e
Keith Smiley <keithbsmiley@gmail.com>
2015-12-03 18:43:13 -0800

    Update github javascript, with label adding

--- js/github.com.js
+++ js/github.com.js
@@ -1,3 +1,4 @@
+// Alternate between h/m Objective-C files with 'a'
 function alternate(URL) {
   var newURL = null;
   if (URL.endsWith(".h")) {
@@ -15,39 +16,6 @@ String.prototype.replaceLast = function(character) {
   return this.substr(0, this.length - 1) + character;
 };
 
-$(document).ready(function() {
-  $(".file-actions").each(function() {
-    var diff = $(this).parent().parent();
-    var link = $("<a href=\"#\" class=\"btn btn-sm\">Hide</a>");
-    $(this).prepend(link);
-    link.click(function() {
-      var codeArea = $(diff).find('[class$="wrapper"]');
-      $(this).html() == 'Hide' ? codeArea.hide() : codeArea.show()
-      $(this).html($(this).html() == 'Show' ? 'Hide' : 'Show');
-      return false;
-    });
-  });
-});
-
-$(document).ready(function() {
-  $(".js-new-blob-submit").remove();
-  $("a[href^=github-mac]").remove();
-  $("button:contains('Update branch')").remove();
-});
-
-$(document).ready(function() {
-  var url = document.URL
-  if (url.toLowerCase().indexOf("lyft") <= 0) {
-    return
-  }
-
-  $(".label.tooltipped").each(function() {
-    if ($(this).text() !== "Approved - Awaiting QA") {
-      $(this).remove();
-    }
-  });
-});
-
 $(document).keypress(function(e) {
   // Exit early if we're in an input field
   // This wasn't needed in Chrome but is in Safari
@@ -62,83 +30,117 @@ $(document).keypress(function(e) {
   }
 });
 
-function selectElements(_this) {
-  _this.actions = commentForm().querySelector('.form-actions');
-  _this.bubblesContent = document.querySelectorAll('.timeline-new-comment.js-comment-container');
-  _this.bubble = _this.bubblesContent[_this.bubblesContent.length - 1];
-  _this.close = _this.actions.querySelector('.js-comment-and-button');
-  _this.comment = _this.actions.querySelector('.primary');
-  return _this.textarea = commentForm().querySelector('textarea');
-}
+// Actions on pageload
 
-function commentForm() {
-  return document.querySelector('.js-new-comment-form');
-}
+$(document).ready(function() {
+  removeDislikedElements();
+  removeMostLabels();
+  addHideDiffButton();
+  addIssueButtons();
+});
 
-function button(text, innerHTML, closable) {
-  if (closable === null) {
-    closable = true;
-  }
-
-  var btn;
-  btn = document.createElement('button');
-  btn.innerHTML = text;
-  btn.className = 'btn';
-  btn.setAttribute('tabindex', '1');
-  btn.setAttribute('type', 'submit');
-  btn.setAttribute('title', innerHTML);
-  btn.addEventListener('click', function(event) {
-    event.preventDefault();
-    selectElements(this);
-    this.textarea.value += " " + innerHTML;
-    if (closable) {
-      this.close.click();
-    } else {
-      this.comment.click();
+// Observe changes to re-add custom issue buttons after page changes
+function registerIssueButtonMutations() {
+  MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  var observer = new MutationObserver(function(mutations, observer) {
+    var buttons = $("#custom_button");
+    if (buttons.length === 0) {
+      addIssueButtons();
     }
-    return this.textarea.value = '';
   });
 
-  return btn;
+  observer.observe(document, {
+    subtree: true,
+    attributes: true
+  });
 }
 
-function insertButtons(elem) {
-  var btn, buttonGroup;
-  buttonGroup = document.createElement('div');
-  buttonGroup.className = 'button-group';
-  buttonGroup.setAttribute('style', 'margin: 10px 0 0 0;');
-  btn = button("Unver", "We cannot update unversioned specs (specs that point to a commit). If you'd like to add a new version the repository would have to be tagged. If you would like to use a different commit in your own project you can use some flags in your `Podfile` documented [here](http://guides.cocoapods.org/syntax/podfile.html#pod)");
-  buttonGroup.appendChild(btn);
-  btn = button("Trunk", "We have just launched 'Trunk', which is the new way for you to submit your Pods. For more information on this have a read through the post at http://blog.cocoapods.org/CocoaPods-Trunk/ If you have previously had your Pod merged into the master repository, please ensure you claim your Pod http://blog.cocoapods.org/Claim-Your-Pods/");
-  buttonGroup.appendChild(btn);
-  btn = button("Merge?", "Can we merge this or mark it as awaiting QA?", false);
-  buttonGroup.appendChild(btn);
-  btn = button("<img src='https://a248.e.akamai.net/assets.github.com/images/icons/emoji/beers.png' width='14' height='14'>", ":beers:", false);
-  buttonGroup.appendChild(btn);
-  btn = button("<img src='https://a248.e.akamai.net/assets.github.com/images/icons/emoji/+1.png' width='14' height='14'>", ":+1:", false);
-  buttonGroup.appendChild(btn);
-  elem.appendChild(buttonGroup);
+// Remove some buttons GitHub adds that I don't want to click
+function removeDislikedElements() {
+  $(".js-new-blob-submit").remove();
+  $("a[href^=github-mac]").remove();
+  $("button:contains('Update branch')").remove();
 }
 
-(function() {
-  if (!commentForm()) {
+// Remove distracting labels from some repos
+function removeMostLabels() {
+  var url = document.URL
+  if (url.toLowerCase().indexOf("lyft/lyft") <= 0) {
     return;
   }
 
-  var mutationObserver, observer;
-  selectElements(this);
-  mutationObserver = typeof WebKitMutationObserver !== "undefined" && WebKitMutationObserver !== null ? WebKitMutationObserver : MutationObserver;
-  observer = new mutationObserver(function(mutations) {
-    return mutations.forEach(function(mutation) {
-      return selectElements(this);
+  $(".label.tooltipped").each(function() {
+    if ($(this).text() !== "Approved - Awaiting QA") {
+      $(this).remove();
+    }
+  });
+}
+
+// Add a button to 'fold' a file in diff mode (super nice for storyboards!)
+function addHideDiffButton() {
+  $(".file-actions").each(function() {
+    var diff = $(this).parent().parent();
+    var link = $("<a href=\"#\" class=\"btn btn-sm\">Hide</a>");
+    $(this).prepend(link);
+    link.click(function() {
+      var codeArea = $(diff).find('[class$="wrapper"]');
+      $(this).html() == 'Hide' ? codeArea.hide() : codeArea.show()
+      $(this).html($(this).html() == 'Show' ? 'Hide' : 'Show');
+      return false;
     });
   });
+}
 
-  observer.observe(this.actions, {
-    childList: true
+function addIssueButtons() {
+  commentContainer = $(".timeline-new-comment");
+  if (commentContainer === undefined) {
+    return;
+  }
+
+  textArea = commentContainer.find("textarea");
+  actions = commentContainer.find(".form-actions #partial-new-comment-form-actions")[0];
+
+  var labelButton = newButton("<img src='https://a248.e.akamai.net/assets.github.com/images/icons/emoji/+1.png' width='14' height='14'>L", ":+1:", textArea);
+  actions.appendChild(configureLabelButton(labelButton));
+  actions.appendChild(newButton("<img src='https://a248.e.akamai.net/assets.github.com/images/icons/emoji/+1.png' width='14' height='14'>", ":+1:", textArea));
+  actions.appendChild(newButton("<img src='https://a248.e.akamai.net/assets.github.com/images/icons/emoji/beers.png' width='14' height='14'>", ":beers:", textArea));
+  actions.appendChild(newButton("Merge?", "Can we merge this or mark it as awaiting QA?", textArea));
+  actions.appendChild(newButton("Trunk", "We have just launched 'Trunk', which is the new way for you to submit your Pods. For more information on this have a read through the post at http://blog.cocoapods.org/CocoaPods-Trunk/ If you have previously had your Pod merged into the master repository, please ensure you claim your Pod http://blog.cocoapods.org/Claim-Your-Pods/", textArea));
+  actions.appendChild(newButton("Unver", "We cannot update unversioned specs (specs that point to a commit). If you'd like to add a new version the repository would have to be tagged. If you would like to use a different commit in your own project you can use some flags in your `Podfile` documented [here](http://guides.cocoapods.org/syntax/podfile.html#pod)", textArea));
+}
+
+// Show the labels dialog, select the right label, close it
+function configureLabelButton(button) {
+  button.addEventListener("click", function(event) {
+    event.stopPropagation();
+
+    setTimeout(function() {
+      $(".sidebar-labels").find("button")[0].click();
+      setTimeout(function() {
+        $('input[type="checkbox"][value="keith"]')[0].click();
+        $("body").click();
+      }, 200);
+    }, 200);
   });
 
-  if (this.close) {
-    insertButtons(this.bubble);
+  return button
+}
+
+// Create generic buttons which change the value of the given textArea
+function newButton(title, actionText, textArea) {
+  if (actionText === null) {
+    actionText = title;
   }
-}).call();
+
+  var button = document.createElement("button");
+  button.innerHTML = title;
+  button.className = "btn";
+  button.id = "custom_button";
+  button.setAttribute("type", "submit");
+  button.setAttribute("title", title);
+  button.addEventListener("click", function(event) {
+    textArea.val(actionText);
+  });
+
+  return button;
+}
