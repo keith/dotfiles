ac08c2c32d33135b4a63bb2e713eb667e2238290
Keith Smiley <keithbsmiley@gmail.com>
2016-01-24 22:27:55 -0800

    Move back to bash gpg-agent config

--- bash/gpg.bash
+++ bash/gpg.bash
@@ -1,9 +1,11 @@
 # Fix gpg not asking for password because it didn't know how
 export GPG_TTY=$(tty)
 
-# gpg-agent configuration
-if [ -f "$HOME/.gpg-agent-info" ]; then
-  source "$HOME/.gpg-agent-info"
-  export GPG_AGENT_INFO
-  export SSH_AUTH_SOCK
+envfile="$HOME/.gpg-agent-info"
+if [[ -e "$envfile" ]] && kill -0 "$(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2)" 2>/dev/null; then
+  eval "$(cat "$envfile")"
+else
+  eval "$(gpg-agent --daemon --enable-ssh-support --write-env-file "$envfile")"
 fi
+export GPG_AGENT_INFO  # the env file does not contain the export statement
+export SSH_AUTH_SOCK   # enable gpg-agent for ssh
