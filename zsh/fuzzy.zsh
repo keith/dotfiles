fccfaa192a66098459520f6ca4d4256cc1902c33
Keith Smiley <keithbsmiley@gmail.com>
2016-02-20 16:30:41 -0800

    Switch to pick in the terminal

--- zsh/fuzzy.zsh
+++ zsh/fuzzy.zsh
@@ -2,27 +2,28 @@ __fsel() {
   command find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
     -o -type f -print \
     -o -type d -print \
-    -o -type l -print 2> /dev/null | sed 1d | cut -b3- | $(__fzfcmd) -m | while read item; do
+    -o -type l -print 2> /dev/null | sed 1d | cut -b3- | $(__fuzzycmd) | while read item; do
     printf '%q ' "$item"
   done
   echo
 }
 
-__fzfcmd() {
-  [ ${FZF_TMUX:-1} -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
+__fuzzycmd() {
+  # [ ${FZF_TMUX:-1} -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
+  echo "pick"
 }
 
 __fselgit() {
-  command git status --short | cut -c4- | $(__fzfcmd) -m
+  command git status --short | cut -c4- | $(__fuzzycmd)
   echo
 }
 
 __fselgitfiles() {
-  command git ls-files `git rev-parse --show-toplevel` --cached --exclude-standard --others | $(__fzfcmd) -m
+  command git ls-files `git rev-parse --show-toplevel` --cached --exclude-standard --others | $(__fuzzycmd)
   echo
 }
 
-fzf-git-files-widget() {
+fuzzy-git-files-widget() {
   result="$(__fselgitfiles)"
   if [[ "$result" != "" ]]; then
     LBUFFER="${LBUFFER}\"$result\""
@@ -30,19 +31,21 @@ fzf-git-files-widget() {
   fi
 }
 
-fzf-git-status-widget() {
+fuzzy-git-status-widget() {
   LBUFFER="${LBUFFER}$(__fselgit)"
   zle redisplay
 }
-zle     -N   fzf-git-status-widget
-bindkey '^F' fzf-git-status-widget
 
-fzf-file-widget() {
+fuzzy-file-widget() {
   LBUFFER="${LBUFFER}$(__fsel)"
   zle redisplay
 }
-zle     -N   fzf-file-widget
-bindkey '^T' fzf-file-widget
 
-zle     -N   fzf-git-files-widget
-bindkey '^G' fzf-git-files-widget
+zle     -N   fuzzy-git-status-widget
+bindkey '^F' fuzzy-git-status-widget
+
+zle     -N   fuzzy-file-widget
+bindkey '^T' fuzzy-file-widget
+
+zle     -N   fuzzy-git-files-widget
+bindkey '^G' fuzzy-git-files-widget
