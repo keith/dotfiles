7877fd55f56f4a5deb12d20bcdfb3f50f3c01f4d
Keith Smiley <keithbsmiley@gmail.com>
2016-05-15 00:32:14 -0700

    Add coloring for context

--- lldbhelpers/context.py
+++ lldbhelpers/context.py
@@ -24,20 +24,38 @@ def output_for_command(debugger, command):
         return ""
 
 
+def blue(string):
+    return "\x1b[34m" + string + "\x1b[39m"
+
+
+def red(string):
+    return "\x1b[31m" + string + "\x1b[39m"
+
+
 def objc_context(debugger, command, result, internal_dict):
     reciever = output_for_command(debugger, "po $rdi").strip()
     selector = selector_from_register(output_for_command(debugger, "x/s $rsi"))
     number_of_arguments = selector.count(":")
     labels = selector.split(":")
-    body = "Reciever: {} Selector: {}\nArgs:\n".format(reciever, selector)
+    body = "Reciever: {} Selector: {}\nArgs:\n".format(blue(reciever),
+                                                       red(selector))
     for i in range(number_of_arguments):
         register = INTEL_ARGS_REGISTERS[i]
         arg = output_for_command(debugger, "po $%s" % register).strip()
-        body += "\n{}: {}\n".format(labels[i], arg)
+        body += "\n{}: {}\n".format(red(labels[i]), arg)
 
-    body += "\n" + output_for_command(debugger, "disassemble -c 5")
+    disassembly = output_for_command(debugger, "disassemble -c 5")
 
-    print(body)
+    try:
+        import pygments.lexers
+        import pygments.formatters
+        disassembly = pygments.highlight(
+                disassembly, pygments.lexers.NasmLexer(),
+                pygments.formatters.TerminalFormatter())
+    except ImportError:
+        pass
+
+    print(body + "\n" + disassembly)
 
 
 def __lldb_init_module(debugger, internal_dict):
