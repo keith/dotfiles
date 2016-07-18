4d5d5f48715fad0b6e463c2bd3f05bf14d31645e
Keith Smiley <keithbsmiley@gmail.com>
2016-07-17 20:27:34 -0700

    Remove unused OS X aliases

--- bash/osx.bash
+++ bash/osx.bash
@@ -1,13 +1,11 @@
 # Make sure we're running OS X
-if [[ "$OSX" != true ]];then
+if [[ "$OSX" != true ]]; then
   return
 fi
 
-# Homebrew cask install apps to global Applications
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-
 # Disable CocoaPods stats ¯\_(ツ)_/¯
 export COCOAPODS_DISABLE_STATS=1
+
 # Disable homebrew analytics
 export HOMEBREW_NO_ANALYTICS=1
 
@@ -19,7 +17,6 @@ alias oo="open ."
 alias pi="pod install --no-repo-update"
 alias psl="pod spec lint *.podspec"
 alias psla="pod spec lint *.podspec --verbose"
-alias pclean="rm -rf \$HOME/Library/Caches/CocoaPods /tmp/CocoaPods"
 
 # Clean up LaunchServices to remove duplicates in the "Open With" menu
 alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
@@ -32,10 +29,6 @@ alias qcleanup="sqlite3 \$HOME/Library/Preferences/com.apple.LaunchServices.Quar
 # Also, clear Apple’s System Logs to improve shell startup speed
 alias emptytrash="sudo rm -rfv /Volumes/\*/.Trashes; sudo rm -rfv \$HOME/.Trash/; sudo rm -rfv /private/var/log/asl/\*.asl"
 alias secureemptytrash="sudo srm -rfv /Volumes/\*/.Trashes; sudo srm -rfv \$HOME/.Trash/; sudo srm -rfv /private/var/log/asl/\*.asl"
-
-# Show/hide hidden files in Finder
-alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true; killall Finder"
-alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false; killall Finder"
 
 # Hide/show all desktop icons (useful when presenting)
 alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
