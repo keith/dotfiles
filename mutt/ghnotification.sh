2071bd80f5d2b44189a1ed6d2aaf00085eaf719f
Keith Smiley <keithbsmiley@gmail.com>
2016-08-11 10:43:25 -0700

    Remove GitHub notification marking read from mutt
    This caused a super annoying problem where if you deleted a message from
    the index, it would write a directory called "<enter-command>..." and
    then start updating offlineimap. I think this was because it was somehow
    interpreting this command as raw key commands. AFAIK there's no way
    around this.

--- mutt/ghnotification.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-beacon_re='https://github\.com/notifications/beacon/.*\.gif'
-beacon_url="$(cat | grep -o "$beacon_re")"
-[ -n "$beacon_url" ] && curl --silent --max-time 3 "$beacon_url" >/dev/null &
