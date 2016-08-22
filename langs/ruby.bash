be5e04baba1045161fd3ee4af89987c0da936be0
Keith Smiley <keithbsmiley@gmail.com>
2016-08-21 18:31:51 -0700

    Conditionally enable chruby

--- langs/ruby.bash
+++ langs/ruby.bash
@@ -2,4 +2,10 @@ export GEM_HOME=$HOME/.gem
 export GEM_PATH=$GEM_HOME
 export PATH=$GEM_HOME/bin:$PATH
 
+CHRUBY="/usr/local/opt/chruby"
+if [[ -d $CHRUBY ]]; then
+  source "$CHRUBY/share/chruby/chruby.sh"
+  chruby "$(chruby | sort | tail -1 | tr -d " *")"
+fi
+
 alias bi="bundle install"
