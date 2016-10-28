5e986320bdffd6143650d7ced02832f00591d789
Keith Smiley <keithbsmiley@gmail.com>
2016-10-28 15:11:54 -0700

    Make hostname be local
    This solves a problem where calling getaddrinfo attempts to get a fully
    qualified domain name for your machine, which is totally unnecessary
    for mutt. This resulted in a lag for the first time you opened mutt (or
    when offline) from between 5 and 30 seconds.

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -21,7 +21,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Set computer name
 name=$1
 sudo scutil --set ComputerName "$name"
-sudo scutil --set HostName "$name"
+sudo scutil --set HostName "$name.local"
 sudo scutil --set LocalHostName "$name"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$name"
 
