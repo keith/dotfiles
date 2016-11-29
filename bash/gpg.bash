81cd1000c1906c1668033316ccfa7e4845e1ecc9
Keith Smiley <keithbsmiley@gmail.com>
2016-11-29 14:29:19 -0800

    Disable gpg-agent ssh support

--- bash/gpg.bash
+++ bash/gpg.bash
@@ -9,7 +9,7 @@ envfile="$HOME/.gpg-agent-info"
 if [ -e "$envfile" ] && kill -0 "$(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2)" 2>/dev/null; then
   eval "$(cat "$envfile")"
 else
-  eval "$(gpg-agent --daemon --enable-ssh-support --write-env-file "$envfile")"
+  eval "$(gpg-agent --daemon --write-env-file "$envfile")"
 fi
 export GPG_AGENT_INFO  # the env file does not contain the export statement
 export SSH_AUTH_SOCK   # enable gpg-agent for ssh
