0a11509e7c27cc493a28925f3610e1c2ad0e38a6
Keith Smiley <keithbsmiley@gmail.com>
2017-04-11 21:39:41 -0700

    Add DOCKER_HOST to bash env

--- bash/env.bash
+++ bash/env.bash
@@ -34,6 +34,9 @@ export MAILDIR="$HOME/.mail"
 # Force gpg-agent to use the current tty
 export GPG_TTY=$(tty)
 
+# Set DOCKER_HOST for Lyft docker infra
+export DOCKER_HOST="tcp://127.0.0.1:2375"
+
 # Source something from all shells for private ENV vars
 if [[ -f "$HOME/.secrets" ]]; then
   source "$HOME/.secrets"
