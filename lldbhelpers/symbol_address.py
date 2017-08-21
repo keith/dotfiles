407bf639aad7cb31c21e528ec5b62183b0073cf5
Keith Smiley <keithbsmiley@gmail.com>
2017-08-21 09:44:43 -0700

    Improve address for symbol lldb script
    There are a few changes here:
    
    1. Filter out Swift input with syntactic sugar. If we wanted to get
       sophisticated here, we could handle these cases, but otherwise these
       don't actually match the symbol name in the binary, so nothing is
       ever found for them.
    
    2. Searching for full symbols, as well as "auto" symbols. See the
       comment about why this is necessary.
    
    3. Fallback to reading the function, as well as the symbol, from the
       found SBSymbolContext. See the comment about why this is necessary.

--- lldbhelpers/symbol_address.py
+++ lldbhelpers/symbol_address.py
@@ -4,12 +4,34 @@ import lldb
 
 def _address_for_symbol(target, symbol_context, include_symbol):
     symbol = symbol_context.GetSymbol()
+    function = symbol_context.GetFunction()
+    start_address = None
+    name = None
+
+    # Try to fetch the symbol's address first, and fallback to the function's
+    # It seems as though symbols contexts fetched with the "full" option (at
+    # least for swift symbols) contain an invalid symbol, but a valid function,
+    # yet functions fetched in other ways, have the opposite, a valid symbol,
+    # and invalid function. Now we check to see if they're valid, fetching the
+    # necessary properties for both, and fallback to failing entirely
+    if symbol.IsValid():
+        start_address = symbol.GetStartAddress()
+        name = symbol.GetName()
+    elif function.IsValid():
+        start_address = function.GetStartAddress()
+        name = function.GetDisplayName()
+    else:
+        raise Exception("Neither function nor symbol is valid")
+
+    if not start_address.IsValid():
+        raise Exception("Start address is invalid")
+
     module = symbol_context.GetModule()
     offset = aslr._aslr_for_module(target, module)
-    address = hex(offset + symbol.GetStartAddress().GetFileAddress())
+    address = hex(offset + start_address.GetFileAddress())
 
     if include_symbol:
-        return "{}: {}".format(symbol.GetName(), address)
+        return "{}: {}".format(name, address)
     else:
         return "{}".format(address)
 
@@ -27,8 +49,30 @@ def address_for_symbol(debugger, command, result, internal_dict):
     """
     # TODO: Allow user to pass module for symbol
     symbol = command.strip()
+    if "?" in symbol:
+        result.AppendWarning("Symbol must not have Swift syntax sugar '{}'"
+                             .format(symbol))
+        result.SetStatus(lldb.eReturnStatusFailed)
+        return
+
     target = debugger.GetSelectedTarget()
-    functions = target.FindFunctions(symbol)
+
+    # Search for functions with both the original, and auto, arguments
+    # The default to this argument is "any" yet it seems as though you
+    # don't actually search for any. For example:
+    # argument | swift symbol                               | result
+    # ---------+--------------------------------------------+-------------
+    # any      | ViewController.viewDidLoad                 | valid symbol
+    # any      | module.ViewController.viewDidLoad () -> () | none
+    # full     | ViewController.viewDidLoad                 | none
+    # full     | module.ViewController.viewDidLoad () -> () | valid symbol
+    #
+    # In this case, I would expect any + a full symbol to still return
+    # the full symbol. This is also supposed to be a bitwise field, so I've
+    # also tried passing multiple options, with no success
+    functions = target.FindFunctions(symbol, lldb.eFunctionNameTypeFull)
+    for symbol_context in target.FindFunctions(symbol):
+        functions.Append(symbol_context)
 
     if len(functions) == 0:
         result.AppendWarning("No symbol found for '{}'".format(symbol))
