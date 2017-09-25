620d8d45f84c825b3b34252ecd72667d9f4eea8e
Keith Smiley <keithbsmiley@gmail.com>
2017-09-24 21:15:31 -0700

    Update youtube comments div id

--- js/youtube.com.js
+++ js/youtube.com.js
@@ -1,4 +1,4 @@
 (function(doc) {
-  var comments = doc.querySelector("#watch-discussion");
+  var comments = doc.querySelector("#comments");
   comments.parentNode.removeChild(comments);
 })(document);
