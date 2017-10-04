296f62a7dab1b0a1ae1592d5c4bd709dee4c5487
Keith Smiley <keithbsmiley@gmail.com>
2017-10-03 21:03:41 -0700

    Add new homebrew python path to $PATH
    Ugh

--- langs/python.bash
+++ langs/python.bash
@@ -1,1 +1,3 @@
+# For homebrew https://discourse.brew.sh/t/what-happened-to-the-standard-python-package/935
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 export PYTHONSTARTUP=$DOTFILES/langs/pystartup
