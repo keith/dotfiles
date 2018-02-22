1c67ce720e07351e38803ca66ebd4bf3b41c7dbe
Keith Smiley <keithbsmiley@gmail.com>
2018-02-22 15:52:57 -0800

    Update python syntax for lldb iblog
    Turns out there's some subtle niceties you can use like this section
    accessor by name.

--- lldbhelpers/iblog.py
+++ lldbhelpers/iblog.py
@@ -17,17 +17,14 @@ def _extract_address_from(memory_string):
 
 
 def _find_section(module, section_name, segment_name):
-    for section in module.section_iter():
-        if section.GetName() != section_name:
-            continue
+    section = module.section[section_name]
+    if not section:
+        raise SectionNotFound()
 
-        for subsection in section:
-            if subsection.GetName() != segment_name:
-                continue
-
-            return subsection
-
-    raise SectionNotFound()
+    try:
+        return next(x for x in section if x.GetName() == segment_name)
+    except StopIteration:
+        raise SectionNotFound()
 
 
 def _find_module(target, name):
@@ -56,12 +53,11 @@ def iblog(debugger, command, context, result, internal_dict):
     load_address = section.GetLoadAddress(target)
     end_address = load_address + section.GetByteSize()
 
-    frame = context.GetFrame()
-    cmd = """memory find --count 1 --string \
+    command = """memory find --count 1 --string \
 'Could not load the "%@" image referenced from a nib in the bundle \
 with identifier "%@"' {} {}""".format(hex(load_address), hex(end_address))
 
-    output = _output_for_command(debugger, cmd)
+    output = _output_for_command(debugger, command)
     string_addr = _extract_address_from(output)
     debugger.HandleCommand(
         "br set --name NSLog --condition '(void *)[$arg1 cString] == {}'"
