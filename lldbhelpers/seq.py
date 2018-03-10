97cec4412561766e2ab2c5adbf57ba7ed3354bef
Keith Smiley <keithbsmiley@gmail.com>
2018-03-10 15:30:12 -0800

    Add lldb seq command
    This allows you to run multiple commands at once

---
+++ lldbhelpers/seq.py
@@ -0,0 +1,21 @@
+import lldb
+import re
+
+
+def seq(debugger, commands, context, result, _):
+    """
+    (lldb) seq command1; command2; ...
+
+    Run multiple separate lldb commands in order, and print the output
+    """
+    interpreter = debugger.GetCommandInterpreter()
+    for command in re.split(r'; *', commands):
+        return_value = lldb.SBCommandReturnObject()
+        interpreter.HandleCommand(command, context, return_value)
+        print >> result, return_value
+        if not return_value.Succeeded():
+            break
+
+
+def __lldb_init_module(debugger, _):
+    debugger.HandleCommand('command script add -f seq.seq seq')
