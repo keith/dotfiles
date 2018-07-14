ab6bd456c91a55339c489867ed8bd9ccf62b424d
Keith Smiley <keithbsmiley@gmail.com>
2018-07-14 15:03:53 -0700

    Switch to pyenv

--- langs/python.bash
+++ langs/python.bash
@@ -1,3 +1,7 @@
-# For homebrew https://discourse.brew.sh/t/what-happened-to-the-standard-python-package/935
-export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 export PYTHONSTARTUP=$DOTFILES/langs/pystartup
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv >/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
