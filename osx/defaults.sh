847e3e8d0198f39c7ce588b9248d8095b07d291d
Keith Smiley <keithbsmiley@gmail.com>
2018-07-14 14:24:54 -0700

    Disable macOS tab to all controls

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -290,9 +290,6 @@ defaults write com.apple.loginwindow PowerButtonSleepsSystem -bool NO
 # Keyboard
 #
 
-# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
