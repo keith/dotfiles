4decb2d4d6d91dca917b716d087b83b9787b311c
Keith Smiley <keithbsmiley@gmail.com>
2018-08-23 18:08:09 -0700

    Remove swiftenv
    I don't need this on macOS

--- langs/swift.bash
+++ langs/swift.bash
@@ -1,9 +1,2 @@
 # Add swiftpm build to path
 export PATH=".git/safe/../../.build/debug:$PATH"
-
-export SWIFTENV_ROOT="$HOME/.swiftenv"
-export PATH="$SWIFTENV_ROOT/bin:$PATH"
-
-if command -v swiftenv >/dev/null; then
-  eval "$(swiftenv init -)"
-fi
