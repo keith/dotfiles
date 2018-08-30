0766d6dba8e085573550ba7f96526c96305cba24
Keith Smiley <keithbsmiley@gmail.com>
2018-08-29 22:28:26 -0700

    Change Xcode build path to build.noindex
    build is too generic, it specifically conflicts with bazel BUILD files,
    which I don't want to ignore. As an added benefit build.noindex is also
    excluded from spotlight and time machine. I'm able to do this since I
    don't ship apps where I care about what's there long term.

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -681,7 +681,7 @@ defaults write com.apple.dt.Xcode AssistantEditorsLayout -int 1
 defaults write com.apple.dt.Xcode IDEEditorCoordinatorTarget_Click -string FocusedEditor
 
 # Use custom derived data location
-defaults write com.apple.dt.Xcode IDECustomDerivedDataLocation -string build
+defaults write com.apple.dt.Xcode IDECustomDerivedDataLocation -string build.noindex
 
 # Show build times in toolbar
 # http://cocoa.tumblr.com/post/131023038113/build-speed
