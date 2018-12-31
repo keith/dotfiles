6f92a69807d399d14224d33007ad2b75115ec57a
Keith Smiley <keithbsmiley@gmail.com>
2018-12-31 15:09:09 -0800

    [rust] Don't error before rust is installed

--- langs/rust.bash
+++ langs/rust.bash
@@ -1,2 +1,8 @@
-source $HOME/.cargo/env
-export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+if [[ ! -d "$HOME/.cargo" ]]; then
+  exit 0
+fi
+
+source "$HOME/.cargo/env"
+
+rust_src="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+export RUST_SRC_PATH="$rust_src"
