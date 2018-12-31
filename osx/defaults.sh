b2e4ad4cb57c7dcc61e50614e8ef5c0afc76158d
Keith Smiley <keithbsmiley@gmail.com>
2018-12-31 14:30:39 -0800

    [defaults] Remove computer name setting

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -5,11 +5,6 @@
 #  sudo fs_usage | grep plist
 #  sudo opensnoop | grep plist
 
-if [[ $# -ne 1 ]];then
-  echo "Usage: ./$(basename "$0") COMPNAME"
-  exit
-fi
-
 killall System\ Preferences
 
 # Ask for the administrator password upfront
@@ -17,13 +12,6 @@ sudo -v
 
 # Keep-alive: update existing 'sudo' time stamp until '.osx' has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Set computer name
-name=$1
-sudo scutil --set ComputerName "$name"
-sudo scutil --set HostName "$name.local"
-sudo scutil --set LocalHostName "$name"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$name"
 
 
 #
