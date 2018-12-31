0577fb4f843e19f989c3e846b43489f3dd9f00d5
Keith Smiley <keithbsmiley@gmail.com>
2018-12-31 14:34:58 -0800

    [defaults] Make TextEdit prefer plain text

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -595,6 +595,12 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 # Show all processes in Activity Monitor
 defaults write com.apple.ActivityMonitor ShowCategory -int 0
 
+# Use plain text mode for new TextEdit documents
+defaults write com.apple.TextEdit RichText -int 0
+# Open and save files as UTF-8 in TextEdit
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
 
 #
 # X11
