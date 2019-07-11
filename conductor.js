7b3b8d6f298193d9a4e46059b7c5afabe1282f30
Keith Smiley <keithbsmiley@gmail.com>
2019-07-10 20:25:41 -0700

    Switch Conductor to Alacritty

--- conductor.js
+++ conductor.js
@@ -103,7 +103,7 @@ api.bind('.', modifiers, Window.bottomRight);
 api.bind('F1', [], function() { Screen.setBrightness(Screen.getBrightness() - 6.25); });
 api.bind('F2', [], function() { Screen.setBrightness(Screen.getBrightness() + 6.25); });
 
-api.bind('y', ["ctrl", "alt"], function() { api.launch("Terminal"); });
+api.bind('y', ["ctrl", "alt"], function() { api.launch("Alacritty"); });
 api.bind('RETURN', ["shift", "cmd"], function() { toggleApp("Messages"); });
 api.bind('a', ["shift", "alt", "cmd"], function() { api.launch("Activity Monitor"); });
 
