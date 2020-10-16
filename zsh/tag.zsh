10939654fcf45fb72a7b4024edf150cd56fb39ca
Keith Smiley <keithbsmiley@gmail.com>
2020-10-15 17:05:27 -0700

    Set root for tag user
    macOS always returns root for me in python and haskell now since
    Catalina

--- zsh/tag.zsh
+++ zsh/tag.zsh
@@ -1,5 +1,5 @@
 if (( $+commands[tag] )); then
-  tag() { command tag "$@" && source /tmp/tag_aliases_$USER 2>/dev/null }
+  tag() { command tag "$@" && source /tmp/tag_aliases_root 2>/dev/null }
   alias ag="echo 'use s'"
   alias s="tag rg"
   alias rg="echo 'use s'"
