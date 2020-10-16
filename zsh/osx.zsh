dcce473de16e266b0d1d4cf78f2e05b72d881c0a
Keith Smiley <keithbsmiley@gmail.com>
2020-10-15 17:07:05 -0700

    Add nix functions
    Not really using nix much yet but doesn't hurt

--- zsh/osx.zsh
+++ zsh/osx.zsh
@@ -5,3 +5,4 @@ fi
 # Link zsh completion files from homebrew
 fpath=(/usr/local/share/zsh/site-functions $fpath)
 fpath=(/usr/local/share/zsh-completions $fpath)
+fpath=($HOME/.nix-profile/share/zsh/site-functions $fpath)
