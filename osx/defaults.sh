e1d376a7b78412493a4b79e3c89c951537bdbf05
Keith Smiley <keithbsmiley@gmail.com>
2020-11-16 17:36:00 -0800

    Open TextEdit directly in a new file

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -604,6 +604,8 @@ defaults write com.apple.TextEdit RichText -int 0
 # Open and save files as UTF-8 in TextEdit
 defaults write com.apple.TextEdit PlainTextEncoding -int 4
 defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+# Open an empty file directly
+defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
 
 
 #
