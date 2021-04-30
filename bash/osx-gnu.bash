40108059e2cf0535ffd97aef2f875ee44460479e
Keith Smiley <keithbsmiley@gmail.com>
2021-04-30 11:20:39 -0700

    Add brew gnu tools back to shell env
    I'm going to move back to brew in general
    
    Partially reverts f4fe4e11915b5b986f799228bea63cbadae6edea

---
+++ bash/osx-gnu.bash
@@ -0,0 +1,33 @@
+if [[ "$OSX" != true ]]; then
+  return
+fi
+
+gnu_manpaths=( \
+  "$BREW_PREFIX/opt/coreutils/libexec/gnuman" \
+  "$BREW_PREFIX/opt/findutils/libexec/gnuman" \
+  "$BREW_PREFIX/opt/gnu-sed/libexec/gnuman" \
+  "$BREW_PREFIX/opt/gnu-tar/libexec/gnuman" \
+)
+
+gnu_bins=( \
+  "$BREW_PREFIX/opt/coreutils/libexec/gnubin" \
+  "$BREW_PREFIX/opt/findutils/libexec/gnubin" \
+  "$BREW_PREFIX/opt/gnu-sed/libexec/gnubin" \
+  "$BREW_PREFIX/opt/gnu-tar/libexec/gnubin" \
+  "$BREW_PREFIX/opt/make/libexec/gnubin" \
+)
+
+new_manpath=""
+for gnu_manpath in "${gnu_manpaths[@]}"
+do
+  new_manpath="$gnu_manpath:$new_manpath"
+done
+
+new_path="$PATH"
+for gnu_bin in "${gnu_bins[@]}"
+do
+  new_path="$gnu_bin:$new_path"
+done
+
+export MANPATH="$new_manpath"
+export PATH="$new_path"
