68a7a39cc0f77ed3f10090c086a0b690ccd840ca
Keith Smiley <keithbsmiley@gmail.com>
2021-04-30 11:20:06 -0700

    Fix chruby with Apple Silicon brew

--- langs/ruby.bash
+++ langs/ruby.bash
@@ -2,9 +2,9 @@ export GEM_HOME=$HOME/.gem
 export GEM_PATH=$GEM_HOME
 export PATH=$GEM_HOME/bin:$PATH
 
-CHRUBY="/usr/local/opt/chruby"
-if [[ -d $CHRUBY ]]; then
-  source "$CHRUBY/share/chruby/chruby.sh"
+readonly chruby_root="$BREW_PREFIX/opt/chruby"
+if [[ -d $chruby_root ]]; then
+  source "$chruby_root/share/chruby/chruby.sh"
   chruby "$(chruby | sort | tail -1 | tr -d " *")"
 fi
 
