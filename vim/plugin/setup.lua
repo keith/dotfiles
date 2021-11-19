689e9f8bed4f1c80f3baeb827cd13437dfa6b9f4
Keith Smiley <keithbsmiley@gmail.com>
2021-11-18 20:18:02 -0800

    Test nvim-cmp
    Currently this is super broken

--- vim/plugin/setup.lua
+++ vim/plugin/setup.lua
@@ -18,7 +18,8 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
   },
 })
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.resolveSupport = {
   properties = {
@@ -62,29 +63,87 @@ require("lsp_signature").on_attach {
   -- },
 }
 
-require("compe").setup {
-  enabled = true,
-  autocomplete = true,
-  debug = false,
-  min_length = 1,
-  preselect = "enable",
-  throttle_time = 80,
-  source_timeout = 200,
-  incomplete_delay = 400,
-  max_abbr_width = 100,
-  max_kind_width = 100,
-  max_menu_width = 100,
-  documentation = true,
+-- require("compe").setup {
+--   enabled = true,
+--   autocomplete = true,
+--   debug = false,
+--   min_length = 1,
+--   preselect = "enable",
+--   throttle_time = 80,
+--   source_timeout = 200,
+--   incomplete_delay = 400,
+--   max_abbr_width = 100,
+--   max_kind_width = 100,
+--   max_menu_width = 100,
+--   documentation = true,
 
-  source = {
-    path = true,
-    buffer = {
-      ignored_filetypes = { "gitconfig", "gitcommit", "gitrebase", "git" },
+--   source = {
+--     path = true,
+--     buffer = {
+--       ignored_filetypes = { "gitconfig", "gitcommit", "gitrebase", "git" },
+--     },
+--     nvim_lsp = true,
+--     nvim_lua = true,
+--   },
+-- }
+-- local cmp = require('cmp')
+
+local cmp = require "cmp"
+cmp.setup {
+  snippet = {
+    --   -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+      --     -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      --     -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+    end,
+  },
+  mapping = {
+    --   ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    --   ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    --   ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    --   ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    --   ['<C-e>'] = cmp.mapping({
+    --     i = cmp.mapping.abort(),
+    --     c = cmp.mapping.close(),
+    --   }),
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
     },
-    nvim_lsp = true,
-    nvim_lua = true,
+  },
+  sources = cmp.config.sources {
+    { name = "nvim_lsp" },
+
+    { name = "nvim_lua" },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+    -- }, {
+    { name = "buffer" },
+    { name = "path" },
   },
 }
+
+-- -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline('/', {
+--   sources = {
+--     { name = 'buffer' }
+--   }
+-- })
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(':', {
+--   sources = cmp.config.sources({
+--     { name = 'path' }
+--   }
+--   -- , {
+--   --   { name = 'cmdline' }
+--   -- }
+--   )
+-- })
 
 function has_highlights(lang)
   local supported = {
