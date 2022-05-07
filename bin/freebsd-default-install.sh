37e93a915b601c414a0b30d42696a14e20bebed7
Keith Smiley <keithbsmiley@gmail.com>
2022-05-06 21:28:40 -0700

    [sh] Add bash freebsd install setup
    Probably not exhaustive, but enough to get an AWS instance up

---
+++ bin/freebsd-default-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+pkg install \
+ bash \
+ cmake \
+ devel/git \
+ fd-find \
+ fzy \
+ git \
+ htop \
+ lsblk \
+ mkfs \
+ neovim \
+ perl5 \
+ readline \
+ readline-dev \
+ ripgrep \
+ sqlite \
+ sqlite3 \
+ subversion \
+ sudo \
+ tmux \
+ vim \
+ zsh
