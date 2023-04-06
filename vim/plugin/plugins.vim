6844fac38eff2b322e22c1c30e75e78b456c90cf
Keith Smiley <keithbsmiley@gmail.com>
2023-04-06 10:39:38 -0700

    [vim] Update illuminate

--- vim/plugin/plugins.vim
+++ vim/plugin/plugins.vim
@@ -16,8 +16,6 @@ let g:sort_motion_flags = 'ui'
 let g:GPGDefaultRecipients = ['0x4C7167F8']
 let g:GPGPreferArmor = 1
 
-let g:Illuminate_delay = 200
-
 " Netrw
 let g:netrw_banner = 0
 let g:netrw_liststyle = 3
--- vim/plugin/setup.lua
+++ vim/plugin/setup.lua
@@ -156,3 +156,15 @@ require("nvim-treesitter.configs").setup {
     },
   },
 }
+
+require("illuminate").configure {
+  providers = {
+    "lsp",
+    "treesitter",
+    "regex",
+  },
+  delay = 200,
+  filetypes_denylist = {
+    "fugitive",
+  },
+}
