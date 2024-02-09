50cff84a35c193c4ab3324a1f1ec572911f435e4
Keith Smiley <keithbsmiley@gmail.com>
2024-02-09 09:10:28 -0800

    [python] Disable pyenv init
    Seems like most of what this does I can do manually, which cuts 100ms
    off my zsh startup time (~50%). Currently this drops completion support,
    but I don't use that often anyways.

--- langs/python.bash
+++ langs/python.bash
@@ -4,6 +4,4 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 # https://github.com/pyenv/pyenv/issues/1649#issuecomment-694388530
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
-if command -v pyenv >/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
+export PYENV_SHELL=zsh
