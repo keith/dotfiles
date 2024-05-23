a8609194e20e7fa3fb7b5403ffc0da6178314202
Keith Smiley <keithbsmiley@gmail.com>
2024-05-22 17:36:18 -0700

    [sh] Add more ubuntu default packages
    This handles a newer node and cmake

--- bin/ubuntu-default-install.sh
+++ bin/ubuntu-default-install.sh
@@ -4,9 +4,13 @@ set -euo pipefail
 
 # TODO: Neovim
 
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt purge --auto-remove cmake lib-nodedev || true
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ jammy main'
 
-sudo apt install \
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
+sudo apt install -y \
   ansifilter \
   bash \
   build-essential \
@@ -16,23 +20,34 @@ sudo apt install \
   fd-find \
   forkstat \
   fzy \
+  gdb \
   gettext \
   git \
   golang \
   htop \
+  kitware-archive-keyring \
   ninja-build \
   nodejs \
+  patchelf \
   ripgrep \
+  speedtest-cli \
   sudo \
   tmux \
+  tree \
   universal-ctags \
   unzip \
+  urlview \
   vim \
   zsh
+
+sudo rm -f /usr/share/keyrings/kitware-archive-keyring.gpg
 
 go install github.com/bazelbuild/bazelisk@latest
 go install github.com/bazelbuild/buildtools/buildifier@latest
 go install github.com/bazelbuild/buildtools/buildozer@latest
 
 sudo npm install -g bash-language-server
-sudo npm install -g pyright
+
+pip3 install cmake-language-server
+pip3 install neovim
+pip3 install pyright
