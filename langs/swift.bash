25fd202f586242fc2f10befb18ae6bdcf31aeeba
Keith Smiley <keithbsmiley@gmail.com>
2025-06-18 22:15:03 -0700

    [sh] Add swiftly env vars
    Not sure if this would be the same on macOS vs linux but I can fix it if
    I ever try it on macOS

---
+++ langs/swift.bash
@@ -0,0 +1,4 @@
+export SWIFTLY_HOME_DIR="$HOME/.local/share/swiftly"
+export SWIFTLY_BIN_DIR="$HOME/.local/share/swiftly/bin"
+export SWIFTLY_TOOLCHAINS_DIR="$HOME/.local/share/swiftly/toolchains"
+export PATH="$SWIFTLY_BIN_DIR:$PATH"
