ba07c2433bb233456ce6057f750fff7c9b0ef75f
Keith Smiley <keithbsmiley@gmail.com>
2026-03-21 17:45:17 -0700

    [sh] Check for ~/.rubies before chruby setup

--- langs/ruby.bash
+++ langs/ruby.bash
@@ -3,7 +3,7 @@ export GEM_PATH=$GEM_HOME
 export PATH=$GEM_HOME/bin:$PATH
 
 readonly chruby_root="$BREW_PREFIX/opt/chruby"
-if [[ -d $chruby_root ]]; then
+if [[ -d $chruby_root && -d ~/.rubies ]]; then
   source "$chruby_root/share/chruby/chruby.sh"
   chruby "$(ls ~/.rubies | sort | head -1)"
 fi
