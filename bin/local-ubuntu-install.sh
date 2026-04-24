a42e0ed85992400a847936dac1e0b9f3a795dce0
Keith Smiley <keithbsmiley@gmail.com>
2026-04-24 15:26:06 -0700

    [sh] Add local ubuntu setup
    Might not be needed for VMs, but nice for desktop configs

---
+++ bin/local-ubuntu-install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# NOTE: This script probably assumes you've already run `ubuntu-default-install.sh`
+
+sudo apt-get install -y \
+  avahi-daemon
+
+user=$USER
+sudo groupadd -f docker
+sudo usermod -a -G docker "$user"
+
+# Setup SysRq key to allow for emergency system commands (e.g., prioritize OOM
+# kill, kill all processes, etc.)
+echo 'kernel.sysrq=1' | sudo tee /etc/sysctl.d/99-sysrq.conf
+sudo sysctl -p /etc/sysctl.d/99-sysrq.conf
+
+# Keep EDITOR for visudo
+# NOTE: Can't have '.' in the file name
+echo 'Defaults:%sudo env_keep += "EDITOR"' | sudo tee /etc/sudoers.d/env_keep_editor
+echo 'Defaults:%sudo env_keep += "SUDO_EDITOR"' | sudo tee -a /etc/sudoers.d/env_keep_editor
+
+# Needed for gpg-agent forwarding
+echo 'StreamLocalBindUnlink yes' | sudo tee /etc/ssh/sshd_config.d/99_stream_local_bind_unlink.conf
+
+systemctl restart sshd
