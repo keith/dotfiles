376a7d0920769df1b96848dbaf209fb51c3ad936
Keith Smiley <keithbsmiley@gmail.com>
2026-04-26 11:24:49 -0700

    [ubuntu] Setup some cpu temp motd stuff

--- bin/local-ubuntu-install.sh
+++ bin/local-ubuntu-install.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 # NOTE: This script probably assumes you've already run `ubuntu-default-install.sh`
 
 sudo apt-get install -y \
-  avahi-daemon
+  avahi-daemon \
+  lm-sensors
 
 user=$USER
 sudo groupadd -f docker
@@ -25,3 +26,12 @@ echo 'Defaults:%sudo env_keep += "SUDO_EDITOR"' | sudo tee -a /etc/sudoers.d/env
 echo 'StreamLocalBindUnlink yes' | sudo tee /etc/ssh/sshd_config.d/99_stream_local_bind_unlink.conf
 
 systemctl restart sshd
+
+cat <<'EOF' | sudo tee /etc/landscape/client.conf
+[sysinfo]
+exclude_sysinfo_plugins = Temperature
+EOF
+
+sudo ln -s "$HOME/.bin/cpu-temp" /etc/update-motd.d/51-custom-temperature
+sudo rm -f /etc/update-motd.d/00-header
+sudo rm -f /etc/update-motd.d/10-help-text
