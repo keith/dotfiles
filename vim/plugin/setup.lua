37b53a425630d6738b435d8b0b9d3bc6db68881b
Keith Smiley <keithbsmiley@gmail.com>
2026-08-12 14:21:22 -0700

    [nvim] Lazy load metals_config

--- vim/plugin/setup.lua
+++ vim/plugin/setup.lua
@@ -122,17 +122,16 @@ vim.lsp.config("sourcekit", {
   filetypes = { "swift", "objc", "objcpp" },
 })
 
-local metals_config = require("metals").bare_config()
-metals_config.capabilities = capabilities
-metals_config.init_options.statusBarProvider = "off"
-metals_config.settings = {
-  serverVersion = "2.0.0-M2",
-  automaticImportBuild = "all",
-}
-
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "scala", "sbt", "java" },
   callback = function()
+    local metals_config = require("metals").bare_config()
+    metals_config.capabilities = capabilities
+    metals_config.init_options.statusBarProvider = "off"
+    metals_config.settings = {
+      serverVersion = "2.0.0-M2",
+      automaticImportBuild = "all",
+    }
     require("metals").initialize_or_attach(metals_config)
   end,
   group = vim.api.nvim_create_augroup("nvim-metals", { clear = true }),
