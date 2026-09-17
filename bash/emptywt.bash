0fdae391dd550d40b01d548640e7ee6a4f2a2b5c
Keith Smiley <keithbsmiley@gmail.com>
2026-09-17 16:40:06 -0700

    [git] Make emptywt take a directory arg

--- bash/emptywt.bash
+++ bash/emptywt.bash
@@ -1,4 +1,9 @@
 emptywt() {
+  if [[ $# -gt 0 && "$1" != -* ]]; then
+    cd -- "$1" || return
+    shift
+  fi
+
   local worktree
   worktree="$("$DOTFILES/bin/emptywt" "$@")" || return
   [[ -n "$worktree" ]] || return
